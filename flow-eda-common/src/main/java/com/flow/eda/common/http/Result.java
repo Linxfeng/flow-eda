48e2956329a43a981bb0bed021dc856e8303dfb7
Linxfeng <1538063845@qq.com>
2022-03-25 11:20:27 +0800

    接口返回时不展示值为null的字段

--- flow-eda-common/src/main/java/com/flow/eda/common/http/Result.java
+++ flow-eda-common/src/main/java/com/flow/eda/common/http/Result.java
@@ -1,5 +1,6 @@
 package com.flow.eda.common.http;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result<T> {
     protected T result;
     protected String error;
