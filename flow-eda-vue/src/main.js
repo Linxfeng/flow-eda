6532182812f265b6095d645896a290d413f00a81
Linxfeng <1538063845@qq.com>
2022-04-01 16:06:14 +0800

    添加click-outside-vue3组件

--- flow-eda-vue/src/main.js
+++ flow-eda-vue/src/main.js
@@ -1,15 +1,17 @@
-import {createApp} from 'vue'
-import router from './router'
-import App from './App.vue'
-import store from './store'
-import './assets/css/icon.css'
-import 'element-plus/lib/theme-chalk/index.css'
-import ElementPlus from 'element-plus'
-import localeZH from 'element-plus/lib/locale/lang/zh-cn'
+import {createApp} from 'vue';
+import router from './router';
+import App from './App.vue';
+import store from './store';
+import './assets/css/icon.css';
+import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus';
+import localeZH from 'element-plus/lib/locale/lang/zh-cn';
+import vClickOutside from "click-outside-vue3";
 
 const app = createApp(App)
 app
     .use(ElementPlus, {locale: localeZH})
+    .use(vClickOutside)
     .use(store)
     .use(router)
     .mount('#app')
