df71735e32e0714c3161366853473fd55715fa63
Linxfeng <1538063845@qq.com>
2022-04-11 17:18:16 +0800

    新增节点类型枚举，用于定义节点类型和节点实例的关系

---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/NodeTypeEnum.java
@@ -0,0 +1,45 @@
+package com.flow.eda.runner.flow.node;
+
+import com.flow.eda.common.exception.InternalException;
+import com.flow.eda.runner.flow.data.FlowData;
+import com.flow.eda.runner.flow.node.end.EndNode;
+import com.flow.eda.runner.flow.node.http.HttpNode;
+import com.flow.eda.runner.flow.node.start.StartNode;
+import com.flow.eda.runner.flow.node.timer.TimerNode;
+import lombok.Getter;
+
+@Getter
+public enum NodeTypeEnum {
+    /** 开始节点 */
+    START("start", StartNode.class),
+    /** 结束节点 */
+    END("end", EndNode.class),
+    /** 定时器 */
+    TIMER("timer", TimerNode.class),
+    HTTP("http", HttpNode.class),
+    ;
+    private final String type;
+    private final Class<? extends Node> clazz;
+
+    NodeTypeEnum(String type, Class<? extends Node> clazz) {
+        this.type = type;
+        this.clazz = clazz;
+    }
+
+    public static NodeTypeEnum getEnumByType(String type) {
+        for (NodeTypeEnum anEnum : values()) {
+            if (anEnum.getType().equals(type)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    public static Class<? extends Node> getClazzByNode(FlowData node) {
+        NodeTypeEnum typeEnum = getEnumByType(node.getType());
+        if (typeEnum == null) {
+            throw new InternalException("cannot find node type " + node.getType());
+        }
+        return typeEnum.getClazz();
+    }
+}
