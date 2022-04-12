4c388a4922701bcee0e7ddf268d2149ccf97467f
Linxfeng <1538063845@qq.com>
2022-04-12 21:56:11 +0800

    新增运行流程功能

--- flow-eda-vue/src/api/nodeData.js
+++ flow-eda-vue/src/api/nodeData.js
@@ -17,3 +17,11 @@ export function setNodeData(body) {
         data: body
     });
 }
+
+// 运行本流程
+export function executeNodeData(flowId) {
+    return request({
+        url: '/api/v1/node/data/deploy?flowId=' + flowId,
+        method: 'post'
+    });
+}
