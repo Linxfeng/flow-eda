4d85727b3c5ec5b3e2c58daea9ad1a7a9606d1b4
Linxfeng <1538063845@qq.com>
2022-04-12 10:38:52 +0800

    完善定时器节点功能，实现执行限制次数后停止

--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/timer/TimerNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/timer/TimerNode.java
@@ -6,9 +6,10 @@ import org.bson.Document;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/** 定时器节点：作为起始节点时，将会定时触发执行 */
 public class TimerNode implements Node {
     private final long period;
     private final TimeUnit unit;
@@ -18,20 +19,35 @@ public class TimerNode implements Node {
         String pd = params.getString("period");
         this.period = Long.parseLong(pd.split(",")[0]);
         this.unit = TimeUnit.valueOf(pd.split(",")[1]);
-        this.times = params.getInteger("times");
+        this.times = Integer.parseInt(params.getString("times"));
     }
 
     @Override
     public void run(NodeFunction function) {
-        this.executeScheduledTask();
-        function.callback(null);
+        this.executeScheduledTask(function);
     }
 
-    private void executeScheduledTask() {
+    /** 执行单个定时器节点，每个节点使用单独的定时线程池 */
+    private void executeScheduledTask(NodeFunction function) {
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-
-        ScheduledFuture<?> future = executor.scheduleAtFixedRate(() -> {}, 0, period, unit);
-
-        // future.cancel();
+        if (this.times > 0) {
+            AtomicInteger n = new AtomicInteger();
+            // 执行times次数后，停止
+            executor.scheduleAtFixedRate(
+                    () -> {
+                        if (n.getAndIncrement() < times) {
+                            function.callback(null);
+                        } else {
+                            // 停止当前定时线程
+                            executor.shutdown();
+                        }
+                    },
+                    0,
+                    period,
+                    unit);
+        } else {
+            // 永久循环，不限制次数
+            executor.scheduleAtFixedRate(() -> function.callback(null), 0, period, unit);
+        }
     }
 }
