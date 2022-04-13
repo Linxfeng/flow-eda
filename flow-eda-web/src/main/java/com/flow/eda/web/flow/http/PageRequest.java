e243a8d8c9baf52bcccf212e5184fecd8c187ed0
Linxfeng <1538063845@qq.com>
2022-04-13 17:33:14 +0800

    将mybatis分页插件从common移至本模块中

---
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/http/PageRequest.java
@@ -0,0 +1,13 @@
+package com.flow.eda.web.flow.http;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PageRequest {
+    /** 默认每页10条 */
+    private int limit = 10;
+    /** 默认从第1页开始 */
+    private int page = 1;
+}
---
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/mybatis/JsonTypeHandler.java
@@ -0,0 +1,65 @@
+package com.flow.eda.web.flow.mybatis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flow.eda.common.exception.InternalException;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+import org.bson.Document;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@MappedTypes(value = {Document.class})
+@MappedJdbcTypes(value = JdbcType.VARCHAR)
+public class JsonTypeHandler extends BaseTypeHandler<Object> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void setNonNullParameter(
+            PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+        try {
+            String s = null;
+            if (parameter != null) {
+                s = objectMapper.writeValueAsString(parameter);
+            }
+            ps.setString(i, s);
+        } catch (JsonProcessingException e) {
+            throw new InternalException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return this.getResult(rs.getString(columnName));
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return this.getResult(rs.getString(columnIndex));
+    }
+
+    @Override
+    public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return this.getResult(cs.getString(columnIndex));
+    }
+
+    private Object getResult(String s) {
+        if (isJson(s)) {
+            try {
+                return objectMapper.readValue(s, Document.class);
+            } catch (JsonProcessingException e) {
+                throw new InternalException(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    private boolean isJson(String s) {
+        return s != null && s.startsWith("{") && s.endsWith("}");
+    }
+}
