92561233938cfab45fd0e44e693edcf3d9ba7c54
Linxfeng <1538063845@qq.com>
2022-04-14 18:03:37 +0800

    新增节点抽象类，约束子类节点的行为和属性

---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/AbstractNode.java
@@ -0,0 +1,36 @@
+package com.flow.eda.runner.flow.node;
+
+import org.bson.Document;
+
+/** 节点抽象类 */
+public abstract class AbstractNode implements Node {
+    /** 节点自定义参数，可传递至下个节点 */
+    private final Document payload;
+    /** 输入参数，由上个节点传递至此 */
+    private final Document input;
+
+    /**
+     * 抽象类含参构造，用于约束子类必须有一个含参构造
+     *
+     * @param params 节点的输入参数，由上一个节点传递至此
+     */
+    public AbstractNode(Document params) {
+        if (params != null) {
+            this.payload = params.get("payload", Document.class);
+            this.input = params.get("input", Document.class);
+            params.remove("payload");
+            params.remove("input");
+        } else {
+            this.payload = null;
+            this.input = null;
+        }
+    }
+
+    public Document getInput() {
+        return input;
+    }
+
+    public Document getPayload() {
+        return new Document("payload", payload);
+    }
+}
--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/Node.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/Node.java
@@ -1,5 +1,11 @@
 package com.flow.eda.runner.flow.node;
 
+/** 节点接口，用于约束所有的子类节点 */
 public interface Node {
-    void run(NodeFunction function);
+    /**
+     * 运行当前节点实例
+     *
+     * @param callback 回调函数，当前节点执行完毕之后进行回调
+     */
+    void run(NodeFunction callback);
 }
