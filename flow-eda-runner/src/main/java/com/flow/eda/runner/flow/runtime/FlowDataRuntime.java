ce908760bc3e0609a7532221ae9c6214ca9c99b3
Linxfeng <1538063845@qq.com>
2022-04-15 17:56:10 +0800

    优化流程运行时的内存占用

--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/runtime/FlowDataRuntime.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/runtime/FlowDataRuntime.java
@@ -8,23 +8,16 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.flow.eda.common.utils.CollectionUtil.*;
+import static com.flow.eda.common.utils.CollectionUtil.filter;
+import static com.flow.eda.common.utils.CollectionUtil.forEach;
 
 @Service
 public class FlowDataRuntime {
     /** 用于单独执行各个流程的线程池 */
     private final ExecutorService threadPool = Executors.newCachedThreadPool();
-    /** 存储所有启用状态的工作流数据 */
-    private final Map<Long, List<FlowData>> flowData = new ConcurrentHashMap<>();
-    /** 存储流程中的所有开始节点 */
-    private final Map<Long, List<FlowData>> startData = new ConcurrentHashMap<>();
-    /** 存储流程中的所有定时节点(作为起始节点) */
-    private final Map<Long, List<FlowData>> timerData = new ConcurrentHashMap<>();
 
     @Autowired private FlowWebSocket ws;
 
@@ -34,29 +27,12 @@ public class FlowDataRuntime {
 
     /** 运行流程 */
     public void runFlowData(List<FlowData> data) {
-        Long flowId = data.get(0).getFlowId();
-        this.putData(data, flowId);
-        // 流数据存储完毕后需要触发定时器节点的执行(异步)
-        List<FlowData> timers = this.timerData.get(flowId);
-        forEach(timers, d -> threadPool.execute(() -> new FlowExecutor(data, ws).start(d)));
-        // 同时需要立即执行开始节点(异步)
-        List<FlowData> starts = this.startData.get(flowId);
+        // 将流数据分为开始节点和定时器节点进行分别执行
+        List<FlowData> starts = filter(data, this::isStartNode);
         forEach(starts, d -> threadPool.execute(() -> new FlowExecutor(data, ws).start(d)));
-    }
-
-    /** 存储流程的流节点数据 */
-    private void putData(List<FlowData> data, Long flowId) {
-        this.flowData.put(flowId, data);
-        // 将流数据分为开始节点和定时器节点进行区分存放
-        List<FlowData> startNodes = filter(data, this::isStartNode);
-        if (isNotEmpty(startNodes)) {
-            this.startData.put(flowId, startNodes);
-        }
         // 过滤掉非起始节点的定时器节点
-        List<FlowData> timerNodes = filter(data, d -> isTimerNode(d) && isStart(d.getId(), data));
-        if (isNotEmpty(timerNodes)) {
-            this.timerData.put(flowId, timerNodes);
-        }
+        List<FlowData> timers = filter(data, d -> isTimerNode(d) && isStart(d.getId(), data));
+        forEach(timers, d -> threadPool.execute(() -> new FlowExecutor(data, ws).start(d)));
     }
 
     private boolean isStartNode(FlowData d) {
