8f12e87a282a097d861258cdf372210c08da1b7f
Linxfeng <1538063845@qq.com>
2022-04-15 17:56:40 +0800

    修复websocket连接/断开问题

--- flow-eda-vue/src/utils/websocket.js
+++ flow-eda-vue/src/utils/websocket.js
@@ -2,7 +2,10 @@ let ws = {};
 
 // 建立连接，监听消息并进行回调
 function onOpen(id, callback) {
-    if (!ws || Object.keys(ws) === 0 || !ws[id]) {
+    if (!ws) {
+        ws = {};
+    }
+    if (Object.keys(ws) === 0 || !ws[id]) {
         const url = 'ws://localhost:8088/ws/flow/node/' + id;
         const socket = new WebSocket(url);
         socket.onmessage = function (msg) {
