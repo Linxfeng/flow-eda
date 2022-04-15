7fef26ea768c90dca11fce155c438a86c136ae2d
Linxfeng <1538063845@qq.com>
2022-04-15 14:09:56 +0800

    新增websocket客户端连接

---
+++ flow-eda-vue/src/utils/websocket.js
@@ -0,0 +1,21 @@
+let ws = {};
+
+// 建立连接，监听消息并进行回调
+function onOpen(id, callback) {
+    if (Object.keys(ws) === 0 || !ws[id]) {
+        const url = 'ws://localhost:8088/ws/flow/node/' + id;
+        const socket = new WebSocket(url);
+        socket.onmessage = function (msg) {
+            callback(msg.data);
+        };
+        ws[id] = socket;
+    }
+}
+
+// 关闭连接
+function onClose() {
+    Object.keys(ws).forEach(k => ws[k].close());
+    ws = null;
+}
+
+export {onOpen, onClose}
