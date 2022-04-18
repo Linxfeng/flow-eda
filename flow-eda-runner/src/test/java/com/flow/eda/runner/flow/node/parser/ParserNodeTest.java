a60f2ca484c229086fa31151bffbaa86e0876bbb
Linxfeng <1538063845@qq.com>
2022-04-18 11:11:00 +0800

    新增解析器节点单元测试

---
+++ flow-eda-runner/src/test/java/com/flow/eda/runner/flow/node/parser/ParserNodeTest.java
@@ -0,0 +1,24 @@
+package com.flow.eda.runner.flow.node.parser;
+
+import org.bson.Document;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ParserNodeTest {
+
+    @Test
+    void run() {
+        Document params = new Document();
+        List<Document> result = new ArrayList<>();
+        Document res = new Document("type", "start");
+        result.add(res);
+        params.append("httpResult", new Document("result", result));
+        params.append("input", new Document("a", "xxx"));
+        params.append("parseKey", "httpResult.result.$0.type,params.a");
+        ParserNode parserNode = new ParserNode(params);
+        parserNode.run((p) -> Assertions.assertTrue(p.containsKey("type") & p.containsKey("a")));
+    }
+}
