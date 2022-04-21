b476e793e01a8acc2b442cfb07c80dea06763dc2
Linxfeng <1538063845@qq.com>
2022-04-21 21:37:14 +0800

    修改解析器节点功能，使用占位符工具类解析值

--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/parser/ParserNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/parser/ParserNode.java
@@ -3,15 +3,18 @@ package com.flow.eda.runner.flow.node.parser;
 import com.flow.eda.runner.flow.node.AbstractNode;
 import com.flow.eda.runner.flow.node.NodeFunction;
 import com.flow.eda.runner.flow.node.NodeVerify;
+import com.flow.eda.runner.flow.utils.PlaceholderUtil;
 import org.bson.Document;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
+import static com.flow.eda.runner.flow.node.NodeVerify.isTrue;
 import static com.flow.eda.runner.flow.node.NodeVerify.notNull;
 
 /** 解析器节点，用于解析上一个节点的输出参数 */
 public class ParserNode extends AbstractNode {
-    private List<String[]> keys;
+    private List<String> keys;
     private Document payload;
 
     public ParserNode(Document params) {
@@ -21,14 +24,7 @@ public class ParserNode extends AbstractNode {
     @Override
     public void run(NodeFunction callback) {
         System.out.println("执行解析器节点");
-        Document result = new Document();
-        for (String[] k : keys) {
-            Object temp = payload.get(k[0]);
-            Object value = getValue(temp, k, 1);
-            if (value != null) {
-                result.append(k[k.length - 1], value);
-            }
-        }
+        Document result = PlaceholderUtil.parse(payload, keys);
         result.append("params", getInput());
         result.putAll(output());
         setStatus(Status.FINISHED);
@@ -43,30 +39,11 @@ public class ParserNode extends AbstractNode {
             String parseKey = params.getString("parseKey");
             notNull(parseKey);
             String[] parseKeys = parseKey.split(",");
-            this.keys = new ArrayList<>();
-            Arrays.stream(parseKeys).map(key -> key.trim().split("\\.")).forEach(keys::add);
+            this.keys = Arrays.asList(parseKeys);
+            isTrue(!keys.isEmpty());
             this.payload = new Document(params).append("params", getInput());
         } catch (Exception ignore) {
             NodeVerify.throwWithName("parseKey");
         }
-    }
-
-    @SuppressWarnings("rawtypes")
-    private Object getValue(Object value, String[] k, int i) {
-        if (i == k.length || value == null) {
-            return value;
-        }
-        Object temp = null;
-        String key = k[i];
-        if (value instanceof Document || value instanceof LinkedHashMap) {
-            temp = ((Map<?, ?>) value).get(key);
-        } else if (value instanceof ArrayList) {
-            if (key.startsWith("$")) {
-                String s = key.substring(1);
-                int index = Integer.parseInt(s);
-                temp = ((ArrayList) value).get(index);
-            }
-        }
-        return getValue(temp, k, ++i);
     }
 }
