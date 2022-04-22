73fff5b69a0659bd06feffb4e5b78bff9a0b6a9d
Linxfeng <1538063845@qq.com>
2022-04-22 16:00:44 +0800

    定时器节点支持cron表达式

--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/timer/TimerNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/node/timer/TimerNode.java
@@ -1,31 +1,40 @@
 package com.flow.eda.runner.flow.node.timer;
 
 import com.flow.eda.common.exception.FlowException;
+import com.flow.eda.common.exception.InvalidParameterException;
 import com.flow.eda.runner.flow.node.AbstractNode;
 import com.flow.eda.runner.flow.node.NodeFunction;
+import lombok.Getter;
 import org.bson.Document;
+import org.springframework.scheduling.support.CronExpression;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.flow.eda.runner.flow.node.NodeVerify.isTrue;
 import static com.flow.eda.runner.flow.node.NodeVerify.notNull;
 
 /** 定时器节点：作为起始节点时，将会定时触发执行 */
+@Getter
 public class TimerNode extends AbstractNode {
     private Long period;
     private TimeUnit unit;
     private Integer times;
+    private String cron;
+    private Runnable runnable;
 
     public TimerNode(Document params) {
         super(params);
     }
 
     @Override
-    public void run(NodeFunction function) {
-        this.executeScheduledTask(function);
+    public void run(NodeFunction callback) {
+        this.runnable = () -> callback.callback(output());
+        // 执行指定次数后，需要停止任务并设置节点状态
+        if (this.times > 0) {
+            TimerTask.runTimes(this, () -> setStatus(Status.FINISHED));
+        } else {
+            TimerTask.run(this);
+        }
     }
 
     @Override
@@ -39,40 +48,22 @@ public class TimerNode extends AbstractNode {
             String pd = params.getString("period");
             notNull(pd, "period");
             isTrue(pd.contains(","), "period");
-            this.period = Long.parseLong(pd.split(",")[0]);
-            isTrue(period > 0, "period");
 
-            this.unit = TimeUnit.valueOf(pd.split(",")[1]);
-            notNull(unit, "period");
+            // 解析cron表达式
+            if ("CRON".equals(pd.split(",")[1])) {
+                String cron = pd.split(",")[0];
+                if (!CronExpression.isValidExpression(cron)) {
+                    throw new InvalidParameterException("The parameter cron expression is invalid");
+                }
+                this.cron = cron;
+            } else {
+                this.period = Long.parseLong(pd.split(",")[0]);
+                isTrue(period > 0, "period");
+                this.unit = TimeUnit.valueOf(pd.split(",")[1]);
+                notNull(unit, "period");
+            }
         } catch (Exception e) {
             throw FlowException.wrap(e, "The timer node parameters is invalid");
-        }
-    }
-
-    /** 执行单个定时器节点，每个节点使用单独的定时线程池 */
-    private void executeScheduledTask(NodeFunction function) {
-        // 执行times次数后，停止
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        AtomicInteger n = new AtomicInteger();
-        if (this.times > 0) {
-            Runnable command =
-                    () -> {
-                        if (n.getAndIncrement() < times) {
-                            System.out.println("执行定时器节点！");
-                            // 最后一次回调时，需要设置运行状态
-                            if (n.get() == times) {
-                                setStatus(Status.FINISHED);
-                            }
-                            function.callback(output());
-                        } else {
-                            // 停止当前定时线程
-                            executor.shutdown();
-                        }
-                    };
-            executor.scheduleAtFixedRate(command, 0, period, unit);
-        } else {
-            // 永久循环，不限制次数
-            executor.scheduleAtFixedRate(() -> function.callback(null), 0, period, unit);
         }
     }
 }
