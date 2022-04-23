7911a2e3a648f72b5ee76f7f78c9b8ff21de150c
Linxfeng <1538063845@qq.com>
2022-04-23 22:10:21 +0800

    新增看门狗，用于监听流程状态变化

---
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/status/FlowStatusWatchDog.java
@@ -0,0 +1,55 @@
+package com.flow.eda.web.flow.status;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+/** 看门狗，用于监听流程状态变化 */
+@Slf4j
+public class FlowStatusWatchDog {
+    /** 看门狗刷新时间 */
+    private static final long SLEEP = 3000;
+    /** key为流程id，节点状态更新时会更新对应的value值为true */
+    private static final Map<String, Boolean> MAP = new ConcurrentHashMap<>();
+
+    public static void refresh(String flowId, Consumer<String> callback) {
+        if (MAP.isEmpty()) {
+            MAP.put(flowId, true);
+            watch(callback);
+        } else {
+            MAP.put(flowId, true);
+        }
+    }
+
+    /** 监视对应的流程是否进行刷新，若固定周期内未刷新，则踢出map并进行回调 */
+    private static void watch(Consumer<String> callback) {
+        Runnable runnable =
+                () -> {
+                    while (!MAP.isEmpty()) {
+                        MAP.keySet()
+                                .forEach(
+                                        flowId -> {
+                                            if (MAP.get(flowId)) {
+                                                MAP.put(flowId, false);
+                                            }
+                                        });
+                        try {
+                            Thread.sleep(SLEEP);
+                        } catch (InterruptedException e) {
+                            log.error(e.getMessage());
+                        }
+                        MAP.keySet()
+                                .forEach(
+                                        flowId -> {
+                                            if (!MAP.get(flowId)) {
+                                                MAP.remove(flowId);
+                                                callback.accept(flowId);
+                                            }
+                                        });
+                    }
+                };
+        new Thread(runnable).start();
+    }
+}
