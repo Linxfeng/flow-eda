0203f354fe150317cf7b681458533b04f9343df7
Linxfeng <1538063845@qq.com>
2022-04-24 17:59:49 +0800

    使用统一管理的线程池运行流程,完善停止流程功能

--- flow-eda-runner/src/main/java/com/flow/eda/runner/flow/runtime/FlowDataRuntime.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/runtime/FlowDataRuntime.java
@@ -1,24 +1,22 @@
 package com.flow.eda.runner.flow.runtime;
 
 import com.flow.eda.common.dubbo.model.FlowData;
+import com.flow.eda.runner.flow.node.Node;
 import com.flow.eda.runner.flow.node.NodeTypeEnum;
 import com.flow.eda.runner.flow.status.FlowWebSocket;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Objects;
 
-import static com.flow.eda.common.utils.CollectionUtil.filter;
-import static com.flow.eda.common.utils.CollectionUtil.forEach;
+import static com.flow.eda.common.utils.CollectionUtil.*;
+import static com.flow.eda.runner.flow.runtime.FlowThreadPool.getThreadPool;
 
 @Service
 public class FlowDataRuntime {
-    /** 用于单独执行各个流程的线程池 */
-    private final ExecutorService threadPool = Executors.newCachedThreadPool();
-
     @Autowired private FlowWebSocket ws;
 
     /** 应用启动时，需要加载出正在运行中的流数据，继续运行 */
@@ -27,12 +25,34 @@ public class FlowDataRuntime {
 
     /** 运行流程 */
     public void runFlowData(List<FlowData> data) {
+        Long flowId =
+                Objects.requireNonNull(findFirst(data, n -> n.getFlowId() != null)).getFlowId();
         // 将流数据分为开始节点和定时器节点进行分别执行
         List<FlowData> starts = filter(data, this::isStartNode);
-        forEach(starts, d -> threadPool.execute(() -> new FlowExecutor(data, ws).start(d)));
+        forEach(
+                starts,
+                d -> getThreadPool(flowId).execute(() -> new FlowExecutor(data, ws).start(d)));
         // 过滤掉非起始节点的定时器节点
         List<FlowData> timers = filter(data, d -> isTimerNode(d) && isStart(d.getId(), data));
-        forEach(timers, d -> threadPool.execute(() -> new FlowExecutor(data, ws).start(d)));
+        forEach(
+                timers,
+                d -> getThreadPool(flowId).execute(() -> new FlowExecutor(data, ws).start(d)));
+    }
+
+    /** 停止流程 */
+    public void stopFlowData(Long flowId) {
+        FlowThreadPool.shutdownThreadPool(flowId);
+        FlowThreadPool.shutdownSchedulerPool(flowId);
+    }
+
+    /** 被中断的节点需要推送中断状态信息 */
+    public void sendNodeInterruptStatus(Long flowId, List<String> nodeIds) {
+        Document status =
+                new Document("status", Node.Status.FAILED.name())
+                        .append("error", "node interrupted");
+        forEach(
+                nodeIds,
+                nodeId -> ws.sendMessage(flowId.toString(), status.append("nodeId", nodeId)));
     }
 
     private boolean isStartNode(FlowData d) {
