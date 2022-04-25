180dd68f470af0898e8124168cccb5ea508583dd
Linxfeng <1538063845@qq.com>
2022-04-25 22:23:50 +0800

    新增EditorBox组件,用于包裹多个编辑器组件

---
+++ flow-eda-vue/src/views/EditorBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="height: 100%;">
+    <editor v-for="item in editorList" v-show="item === flowId" :key="item" :flowId="item"></editor>
+  </div>
+</template>
+
+<script>
+import {computed} from "vue";
+import {useStore} from "vuex";
+import {useRoute} from "vue-router";
+import Editor from "./Editor.vue";
+
+export default {
+  name: "EditorBox",
+  components: {
+    Editor
+  },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+
+    const flowId = Number.parseInt(route.query.flowId);
+    const editorList = computed(() => store.state.editorList).value;
+
+    return {
+      flowId,
+      editorList
+    };
+  },
+};
+</script>
