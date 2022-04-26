83e66e8799fa9d26a872c9908af2f03ffa5bfa98
Linxfeng <1538063845@qq.com>
2022-04-26 14:58:16 +0800

    更新流程状态看门狗刷新周期为2s

--- flow-eda-web/src/main/java/com/flow/eda/web/flow/status/FlowStatusWatchDog.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/status/FlowStatusWatchDog.java
@@ -10,10 +10,11 @@ import java.util.function.Consumer;
 @Slf4j
 public class FlowStatusWatchDog {
     /** 刷新周期 */
-    private static final long SLEEP = 3000;
+    private static final long SLEEP = 2000;
     /** key为流程id，节点状态更新时会更新对应的value值为true */
     private static final Map<String, Boolean> MAP = new ConcurrentHashMap<>();
 
+    /** 刷新看门狗 */
     public static void refresh(String flowId, Consumer<String> callback) {
         if (MAP.isEmpty()) {
             MAP.put(flowId, true);
@@ -50,7 +51,7 @@ public class FlowStatusWatchDog {
                                         });
                     }
                 };
-        // 显式创建单独线程去执行监视，若没有任务，此线程将被销毁
+        // 显式创建单独线程去执行监视任务，若任务执行完毕，此线程将被销毁
         new Thread(runnable).start();
     }
 }
