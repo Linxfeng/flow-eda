5d2e340ad96c5b9b2ca48b2a952ab0c34ec68248
Linxfeng <1538063845@qq.com>
2022-04-28 16:04:33 +0800

    更新mq仅接收流程状态变化信息

--- flow-eda-web/src/main/java/com/flow/eda/web/flow/status/FlowStatusListener.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/status/FlowStatusListener.java
@@ -6,11 +6,8 @@ import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 public class FlowStatusListener {
@@ -19,11 +16,10 @@ public class FlowStatusListener {
     @RabbitListener(
             bindings =
                     @QueueBinding(
-                            value = @Queue(value = "flow_node_status_updated"),
-                            exchange = @Exchange("flow.node.status"),
-                            key = "flow.node.status.updated"))
-    public void onRabbitMessage(@Payload Document payload, @Headers Map<String, Object> headers) {
-        payload.append("timestamp", headers.get("timestamp"));
-        flowStatusService.updateStatus(payload);
+                            value = @Queue(value = "flow_status_updated"),
+                            exchange = @Exchange("flow.status"),
+                            key = "flow.status.updated"))
+    public void onRabbitMessage(@Payload Document payload) {
+        flowStatusService.update(payload);
     }
 }
