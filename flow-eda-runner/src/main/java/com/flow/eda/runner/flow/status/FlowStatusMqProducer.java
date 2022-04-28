d600eb5f568da0846547ba6d5825e291a07a573f
Linxfeng <1538063845@qq.com>
2022-04-28 14:52:55 +0800

    向mq中发送流程的实时运行状态

---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/flow/status/FlowStatusMqProducer.java
@@ -0,0 +1,25 @@
+package com.flow.eda.runner.flow.status;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FlowStatusMqProducer {
+    private static final String EXCHANGE = "flow.status";
+    private static final String ROUTING_KEY = "flow.status.updated";
+    @Autowired private RabbitTemplate rabbitTemplate;
+
+    /** 向mq中发送流程的实时运行状态 */
+    public void sendFlowStatus(String flowId, String status) {
+        try {
+            Document payload = new Document("flowId", flowId).append("status", status);
+            rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, payload);
+        } catch (Exception e) {
+            log.error("send flow status to rabbitmq failed:{}", e.getMessage());
+        }
+    }
+}
