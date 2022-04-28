e3aad1e793db22213d74b8b259b85da6620bcce7
Linxfeng <1538063845@qq.com>
2022-04-28 16:47:39 +0800

    更新websocket为每个流程单个连接

--- flow-eda-vue/src/utils/websocket.js
+++ flow-eda-vue/src/utils/websocket.js
@@ -1,46 +1,26 @@
-let flowWs = {};
-let nodeWs = {};
+let ws = {};
 
-// 监听流程状态信息
-async function onOpenFlow(id, callback) {
-    if (Object.keys(flowWs) === 0 || !flowWs[id]) {
-        const url = 'ws://localhost:8088/ws/flow/status/' + id;
-        const socket = new WebSocket(url);
-        socket.onmessage = function (msg) {
-            callback(msg.data);
-        };
-        flowWs[id] = socket;
-    }
-}
-
-// 监听节点信息
-async function onOpenNode(id, callback) {
-    if (Object.keys(nodeWs) === 0 || !nodeWs[id]) {
+// 建立连接，监听消息并进行回调
+async function onOpen(id, callback) {
+    if (Object.keys(ws) === 0 || !ws[id]) {
         const url = 'ws://localhost:8088/ws/flow/' + id + '/nodes';
         const socket = new WebSocket(url);
         socket.onmessage = function (msg) {
             callback(msg.data);
         };
-        nodeWs[id] = socket;
+        ws[id] = socket;
     }
 }
 
 // 关闭连接
 function onClose(id) {
-    if (nodeWs && nodeWs[id]) {
+    if (ws && ws[id]) {
         try {
-            nodeWs[id].close();
+            ws[id].close();
         } catch (ignore) {
         }
-        nodeWs[id] = null;
-    }
-    if (flowWs && flowWs[id]) {
-        try {
-            flowWs[id].close();
-        } catch (ignore) {
-        }
-        flowWs[id] = null;
+        ws[id] = null;
     }
 }
 
-export {onOpenFlow, onOpenNode, onClose}
+export {onOpen, onClose}
