6bd1908cd7a68f163c8d4912a3a4edaff8dc580b
Linxfeng <1538063845@qq.com>
2022-04-29 16:26:19 +0800

    去除节点类型的增删操作，暂不开放操作接口

--- flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeTypeMapper.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeTypeMapper.java
@@ -1,25 +1,12 @@
 package com.flow.eda.web.flow.node.type;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface NodeTypeMapper {
-
-    @Insert(
-            "INSERT INTO eda_flow_node_type (type,type_name,svg,background,description) "
-                    + "VALUES (#{type},#{typeName},#{svg},#{background},#{description})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(NodeType nodeType);
-
-    @Select(
-            "<script>SELECT * FROM eda_flow_node_type WHERE 1=1 "
-                    + "<if test='name!=null'> AND type_name LIKE '%${name}%'</if></script>")
-    List<NodeType> findByName(String name);
 
     @Select(
             "<script>SELECT * FROM eda_flow_node_type WHERE id IN "
--- flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeTypeService.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeTypeService.java
@@ -1,9 +1,7 @@
 package com.flow.eda.web.flow.node.type;
 
-import com.flow.eda.web.flow.node.data.NodeRequest;
 import com.flow.eda.web.flow.node.type.param.NodeTypeParam;
 import com.flow.eda.web.flow.node.type.param.NodeTypeParamMapper;
-import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +13,6 @@ import static com.flow.eda.common.utils.CollectionUtil.*;
 public class NodeTypeService {
     @Autowired private NodeTypeMapper nodeTypeMapper;
     @Autowired private NodeTypeParamMapper nodeTypeParamMapper;
-
-    public NodeType addNodeType(NodeType nodeType) {
-        nodeTypeMapper.insert(nodeType);
-        return nodeType;
-    }
-
-    public List<NodeType> listByPage(NodeRequest request) {
-        PageHelper.startPage(request.getPage(), request.getLimit());
-        List<NodeType> list = nodeTypeMapper.findByName(request.getName());
-        return mergeNodeTypeParam(list);
-    }
 
     public List<NodeType> findByIds(List<Long> ids) {
         List<NodeType> list = nodeTypeMapper.findByIds(ids);
