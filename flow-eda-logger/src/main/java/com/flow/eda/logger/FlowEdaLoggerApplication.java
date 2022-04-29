c2c05220562bcf2397a7e922787d60d004ee850d
Linxfeng <1538063845@qq.com>
2022-04-29 20:33:43 +0800

    新增日志管理模块

---
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.flow.eda.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowEdaLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowEdaLoggerApplication.class, args);
+    }
+}
---
+++ flow-eda-logger/src/test/java/com/flow/eda/logger/FlowEdaLoggerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.flow.eda.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowEdaLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
