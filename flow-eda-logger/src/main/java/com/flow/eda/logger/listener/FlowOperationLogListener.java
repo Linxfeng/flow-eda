98d266197b1b015f53247e04389d3f89a752f4d9
Linxfeng <1538063845@qq.com>
2022-05-01 22:55:33 +0800

    接收操作日志并进行存储写入

--- flow-eda-logger/src/main/java/com/flow/eda/logger/listener/FlowOperationLogListener.java
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/listener/FlowOperationLogListener.java
@@ -1,15 +1,18 @@
 package com.flow.eda.logger.listener;
 
+import com.flow.eda.logger.writer.LogFileWriter;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /** 接收业务操作日志 */
 @Component
 public class FlowOperationLogListener {
+    @Autowired private LogFileWriter logFileWriter;
 
     @RabbitListener(
             bindings =
@@ -18,6 +21,6 @@ public class FlowOperationLogListener {
                             exchange = @Exchange("flow.log"),
                             key = "flow.operation.log"))
     public void onRabbitMessage(@Payload String message) {
-        System.out.println(message);
+        logFileWriter.writeOperationLogs(message);
     }
 }
