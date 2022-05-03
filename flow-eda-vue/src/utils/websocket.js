9d1abd1c097fba0a2cca8efb177280ffc66e11d7
Linxfeng <1538063845@qq.com>
2022-05-03 12:19:37 +0800

    建立websocket连接，接收实时运行日志

--- flow-eda-vue/src/utils/websocket.js
+++ flow-eda-vue/src/utils/websocket.js
@@ -1,8 +1,9 @@
 let ws = {};
+let logWs = {};
 
 // 建立连接，监听消息并进行回调
 async function onOpen(id, callback) {
-    if (Object.keys(ws) === 0 || !ws[id]) {
+    if (Object.keys(ws).length === 0 || !ws[id]) {
         const url = 'ws://localhost:8088/ws/flow/' + id + '/nodes';
         const socket = new WebSocket(url);
         socket.onmessage = function (msg) {
@@ -23,4 +24,26 @@ function onClose(id) {
     }
 }
 
-export {onOpen, onClose}
+// 监听日志消息
+function onOpenLogs(id, callback) {
+    if (Object.keys(logWs).length === 0 || !logWs[id]) {
+        const url = 'ws://localhost:8082/ws/flow/' + id + '/logs';
+        const socket = new WebSocket(url);
+        socket.onmessage = function (msg) {
+            callback(msg.data);
+        };
+        logWs[id] = socket;
+    }
+}
+
+function onCloseLogs(id) {
+    if (logWs && logWs[id]) {
+        try {
+            logWs[id].close();
+        } catch (ignore) {
+        }
+        logWs[id] = null;
+    }
+}
+
+export {onOpen, onClose, onOpenLogs, onCloseLogs}
