f770b69ddf4a13f39e0857baf0dbb66c535ecb3c
Linxfeng <1538063845@qq.com>
2022-05-03 14:45:50 +0800

    日志栏自动滚动，添加自定义拖拽高度

--- flow-eda-vue/src/components/editor/FlowLog.vue
+++ flow-eda-vue/src/components/editor/FlowLog.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="flow-log">
+  <div id="flow-log" class="flow-log">
+    <div v-dragging class="drag-box"></div>
     <Codemirror
         v-model:value="logContent"
-        :KeepCursorInEnd="true"
         :options="{mode: 'javascript', styleActiveLine: true, theme: 'dracula', readOnly: true}"
         style="font-size: 14px;"
+        @change="onChange"
     />
   </div>
 </template>
@@ -22,18 +23,51 @@ export default {
   components: {
     Codemirror
   },
+  directives: {
+    // 自定义拖拽指令
+    dragging(el) {
+      let targetDiv = document.getElementById('flow-log')
+      el.onmousedown = function () {
+        document.onmousemove = function (e) {
+          let th = document.body.clientHeight - e.clientY
+          if (th < 100) {
+            th = 100
+          }
+          targetDiv.style.height = th + "px";
+        }
+        document.onmouseup = function () {
+          document.onmousemove = null;
+          document.onmouseup = null;
+        };
+        return false;
+      };
+    }
+  },
   setup() {
 
-    return {};
+    // 自动滚动到底部
+    const onChange = (v, cm) => {
+      const nowScrollInfo = cm.getScrollInfo();
+      cm.scrollTo(nowScrollInfo.left, nowScrollInfo.height);
+    };
+
+    return {
+      onChange
+    };
   }
 };
 </script>
 
 <style>
 .flow-log {
-  height: 400px;
+  height: 340px;
   width: 96%;
   position: fixed;
   bottom: 0;
+}
+
+.drag-box {
+  height: 6px;
+  cursor: n-resize;
 }
 </style>
