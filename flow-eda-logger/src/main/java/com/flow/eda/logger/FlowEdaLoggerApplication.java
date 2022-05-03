169e51d027374c673dd981c53c14db1d15168526
Linxfeng <1538063845@qq.com>
2022-05-03 12:23:01 +0800

    配置websoket

--- flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
@@ -2,11 +2,19 @@ package com.flow.eda.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class FlowEdaLoggerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowEdaLoggerApplication.class, args);
+    }
+
+    /** websocket config */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
