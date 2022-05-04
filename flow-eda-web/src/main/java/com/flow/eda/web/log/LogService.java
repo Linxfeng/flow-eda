fdc7f6fa7e88153abeed0abcd7352963a9de117a
Linxfeng <1538063845@qq.com>
2022-05-04 19:04:45 +0800

    查询日志信息列表，聚合流程名称

---
+++ flow-eda-web/src/main/java/com/flow/eda/web/log/LogService.java
@@ -0,0 +1,34 @@
+package com.flow.eda.web.log;
+
+import com.flow.eda.common.dubbo.api.LogsService;
+import com.flow.eda.common.dubbo.model.Logs;
+import com.flow.eda.common.utils.CollectionUtil;
+import com.flow.eda.common.utils.MergeBuilder;
+import com.flow.eda.web.flow.Flow;
+import com.flow.eda.web.flow.FlowMapper;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.flow.eda.common.utils.CollectionUtil.filterMap;
+
+@Service
+public class LogService {
+    @DubboReference private LogsService logsService;
+    @Autowired private FlowMapper flowMapper;
+
+    public List<Logs> getLogList(LogRequest request) {
+        List<Logs> list = logsService.getLogList(request.getType());
+        List<String> ids = filterMap(list, l -> l.getFlow() != null, Logs::getFlow);
+        if (CollectionUtil.isNotEmpty(ids)) {
+            // 聚合流程名称
+            List<Flow> flows = flowMapper.findByIds(ids);
+            return MergeBuilder.source(list, Logs::getFlow)
+                    .target(flows, Flow::getId)
+                    .mergeS((log, flow) -> log.setFlowName(flow.getName()));
+        }
+        return list;
+    }
+}
