1726f00073e1c3eaacc9443797bb22a8fea97c5d
Linxfeng <1538063845@qq.com>
2022-05-11 16:46:48 +0800

    更新国际化文件

--- flow-eda-react/src/locales/en-US/component.ts
+++ flow-eda-react/src/locales/en-US/component.ts
@@ -2,6 +2,8 @@ export default {
   'component.modalForm.confirm': 'Confirm',
   'component.modalForm.cancel': 'Cancel',
   'component.modalForm.confirm.title': 'Are you sure delete?it cannot be recovered!',
+  'component.message.loading': 'Operating...',
+  'component.message.success': 'Successful operation',
 
 
 
--- flow-eda-react/src/locales/zh-CN/component.ts
+++ flow-eda-react/src/locales/zh-CN/component.ts
@@ -2,6 +2,8 @@ export default {
   'component.modalForm.confirm': '确认',
   'component.modalForm.cancel': '取消',
   'component.modalForm.confirm.title': '确认要删除吗？删除后将无法恢复！',
+  'component.message.loading': '操作中...',
+  'component.message.success': '操作成功',
 
 
 
