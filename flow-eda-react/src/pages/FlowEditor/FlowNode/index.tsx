3bf6e716beca62eb1d158f60dedff352f83cc220
Linxfeng <1538063845@qq.com>
2022-05-16 21:19:53 +0800

    更新编辑器节点组件内容

--- flow-eda-react/src/pages/FlowEditor/FlowNode/index.tsx
+++ flow-eda-react/src/pages/FlowEditor/FlowNode/index.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './index.less';
 
-const FlowNode: React.FC<{ node: any }> = (props) => {
-  const imgStyle = { padding: '4px' };
+const FlowNode: React.FC<{
+  node: API.Node;
+  changeLineState: (nodeId: string, show: boolean) => void;
+  showNodeDetail: (node: API.Node) => void;
+}> = (props) => {
   const node = props.node;
+  const [mouseEnter, setMouseEnter] = useState<boolean>(false);
+  const [active, setActive] = useState<boolean>(false);
+  const [selected, setSelected] = useState<boolean>(false);
+  const [clickedOutside, setClickedOutside] = useState(false);
+  const className = active || selected ? 'node-item active' : 'node-item';
+  const nodeRef = useRef<any>();
+
+  const setActiveNode = (e: any) => {
+    setClickedOutside(false);
+    if (e.ctrlKey) {
+      setSelected(!selected);
+    } else {
+      setActive(true);
+      setSelected(false);
+      setTimeout(() => {
+        props.changeLineState(props.node.id, true);
+        props.showNodeDetail(props.node);
+      }, 0);
+    }
+    console.log(clickedOutside);
+  };
+
+  const setNotActive = (e: MouseEvent) => {
+    if (!e.ctrlKey) {
+      setSelected(false);
+    }
+    if (!active) {
+      return;
+    }
+    props.changeLineState(props.node.id, false);
+    setActive(false);
+  };
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (nodeRef?.current && e.target && !nodeRef.current.contains(e.target)) {
+      setClickedOutside(true);
+      setNotActive(e);
+    }
+    console.log(clickedOutside);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
 
   return (
-    <div id="node" className="node-item" style={{ background: node.background }}>
+    <div
+      ref={nodeRef}
+      id="node"
+      className={className}
+      onClick={(e) => setActiveNode(e)}
+      onMouseEnter={() => setMouseEnter(true)}
+      onMouseLeave={() => setMouseEnter(false)}
+      style={{ top: node.top, left: node.left, background: node.nodeType?.background }}
+    >
       <div className="node-svg">
-        <img src={node.nodeType.svg} alt="" style={imgStyle} />
+        <img src={node.nodeType?.svg} alt="" style={{ padding: '4px' }} />
       </div>
       <div className="node-name">{node.nodeName}</div>
       {node.status === 'RUNNING' && (
@@ -26,10 +82,10 @@ const FlowNode: React.FC<{ node: any }> = (props) => {
           <img alt="运行失败" src="/svg/status/failed.svg" title={node.error} />
         </div>
       )}
-      <div className="node-anchor anchor-top" />
-      <div className="node-anchor anchor-right" />
-      <div className="node-anchor anchor-bottom" />
-      <div className="node-anchor anchor-left" />
+      {mouseEnter && <div className="node-anchor anchor-top" />}
+      {mouseEnter && <div className="node-anchor anchor-right" />}
+      {mouseEnter && <div className="node-anchor anchor-bottom" />}
+      {mouseEnter && <div className="node-anchor anchor-left" />}
     </div>
   );
 };
