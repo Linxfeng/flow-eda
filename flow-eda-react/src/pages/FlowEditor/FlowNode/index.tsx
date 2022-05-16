3507a2f431e4f0339600b453a10533db4a470525
Linxfeng <1538063845@qq.com>
2022-05-16 11:27:26 +0800

    完善节点组件页面

--- flow-eda-react/src/pages/FlowEditor/FlowNode/index.tsx
+++ flow-eda-react/src/pages/FlowEditor/FlowNode/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import './index.less';
-import svg from '/public/svg/start.svg';
 
-const FlowNode: React.FC = () => {
+const FlowNode: React.FC<{ node: any }> = (props) => {
   const imgStyle = { padding: '4px' };
+  const node = props.node;
 
   return (
-    <div id="node" className="node-item" style={{ background: 'rgba(0, 128, 0, 0.2)' }}>
+    <div id="node" className="node-item" style={{ background: node.background }}>
       <div className="node-svg">
-        <img src={svg} alt="" style={imgStyle} />
+        <img src={node.nodeType.svg} alt="" style={imgStyle} />
       </div>
-      <div className="node-name">节点名称</div>
+      <div className="node-name">{node.nodeName}</div>
+      {node.status === 'RUNNING' && (
+        <div className="node-status">
+          <img alt="运行中" src="/svg/status/running.svg" title="运行中..." />
+        </div>
+      )}
+      {node.status === 'FINISHED' && (
+        <div className="node-status">
+          <img alt="运行完成" src="/svg/status/finished.svg" title="运行完成" />
+        </div>
+      )}
+      {node.status === 'FAILED' && (
+        <div className="node-status">
+          <img alt="运行失败" src="/svg/status/failed.svg" title={node.error} />
+        </div>
+      )}
       <div className="node-anchor anchor-top" />
       <div className="node-anchor anchor-right" />
       <div className="node-anchor anchor-bottom" />
