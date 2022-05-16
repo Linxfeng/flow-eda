45058c74d0aba03eb7f1f09fbc8579a108b82071
Linxfeng <1538063845@qq.com>
2022-05-16 16:24:11 +0800

    封装图标组件，引入iconfont图标

---
+++ flow-eda-react/src/components/IconFont/index.tsx
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: ['https://at.alicdn.com/t/font_3309897_kldx42pzbc.js'],
+});
+
+export default IconFont;
