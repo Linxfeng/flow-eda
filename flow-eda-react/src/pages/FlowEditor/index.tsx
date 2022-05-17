9f8376855f44cbd59636a7622702469baa96f8bb
Linxfeng <1538063845@qq.com>
2022-05-17 11:06:54 +0800

    更新编辑器组件

--- flow-eda-react/src/pages/FlowEditor/index.tsx
+++ flow-eda-react/src/pages/FlowEditor/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'umi';
-import jsplumb from 'jsplumb';
+import { jsPlumb } from 'jsplumb';
 import panzoom from 'panzoom';
-import { deleteFlow, getFlowData, getNodeTypes } from '@/services/api';
+import { getFlowData, getNodeTypes } from '@/services/api';
 import {
   jsplumbConnectOptions,
   jsplumbSetting,
@@ -21,8 +21,8 @@ import { useFormatMessage } from '@/hooks';
 const FlowEditor: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { formatMsg } = useFormatMessage();
-  const [jsPlumbInstance] = useState(jsplumb.jsPlumb.getInstance());
-  const [data, setDate] = useState<{ nodeList: API.Node[]; lineList: API.Node[] }>({
+  const [jsPlumbInstance] = useState(jsPlumb.getInstance());
+  const [data] = useState<{ nodeList: API.Node[]; lineList: API.Node[] }>({
     nodeList: [],
     lineList: [],
   });
@@ -53,21 +53,17 @@ const FlowEditor: React.FC = () => {
   const [currentItem, setCurrentItem] = useState<API.NodeType>();
 
   /** 初始化节点数据 */
-  async function initFlowData() {
+  const initFlowData = async () => {
     const res = await getFlowData(id);
-    const node: API.Node[] = [];
-    const line: API.Node[] = [];
-    if (res?.result) {
-      res.result.map((d) => {
-        if (d.nodeName) {
-          node.push(d);
-        } else {
-          line.push(d);
-        }
-      });
-    }
-    setDate({ nodeList: node, lineList: line });
-  }
+    res?.result?.map((d: API.Node) => {
+      if (d.nodeName) {
+        data.nodeList.push(d);
+      } else {
+        data.lineList.push(d);
+      }
+      setFlowStatus('FINISHED');
+    });
+  };
 
   /** 初始化节点类型 */
   const initNodeType = async () => {
@@ -99,6 +95,7 @@ const FlowEditor: React.FC = () => {
 
   /** 注册节点拖动节点事件 */
   const draggableNode = (nodeId: string) => {
+    console.log('draggableNode:' + nodeId);
     jsPlumbInstance.draggable(nodeId, {
       drag: (params) => {
         alignForLine(nodeId, params.pos);
@@ -118,51 +115,44 @@ const FlowEditor: React.FC = () => {
 
   /** 加载流程图 */
   const loadEasyFlow = () => {
-    console.log("loadEasyFlow")
-    const nodes = data.nodeList;
-    if (nodes && nodes.length > 0) {
-      nodes.forEach((node) => {
-        // 设置源点，可以拖出线连接其他节点
-        jsPlumbInstance.makeSource(node.id, jsplumbSourceOptions);
-        // 设置目标点，其他源点拖出的线可以连接该节点
-        jsPlumbInstance.makeTarget(node.id, jsplumbTargetOptions);
-        // 注册节点拖动事件
-        draggableNode(node.id);
-      });
+    console.log('loadEasyFlow');
+    for (let i = 0; i < data.nodeList.length; i++) {
+      const node = data.nodeList[i];
+      console.log(node);
+      // 设置源点，可以拖出线连接其他节点
+      jsPlumbInstance.makeSource(node.id, jsplumbSourceOptions);
+      // 设置目标点，其他源点拖出的线可以连接该节点
+      jsPlumbInstance.makeTarget(node.id, jsplumbTargetOptions);
+      // 注册节点拖动事件
+      draggableNode(node.id);
     }
     //取消连接事件
     jsPlumbInstance.unbind('connection');
-    const lines = data.lineList;
-    if (lines && lines.length > 0) {
-      // 初始化连线
-      lines.forEach((line) => {
-        console.log(line)
-        jsPlumbInstance.connect(
-          {
-            source: line.from,
-            target: line.to,
-          },
-          jsplumbConnectOptions,
-        );
-      });
+    for (let i = 0; i < data.lineList.length; i++) {
+      const line = data.lineList[i];
+      jsPlumbInstance.connect(
+        {
+          source: line.from,
+          target: line.to,
+        },
+        jsplumbConnectOptions,
+      );
     }
     //注册连接事件
     jsPlumbInstance.bind('connection', (evt) => {
-      console.log("connection")
-      const line: API.Node = {
+      console.log('connection');
+      data.lineList.push({
         id: generateUniqueID(8),
         flowId: id,
         from: evt.source.id,
         to: evt.target.id,
-      };
-      if (lines) {
-        lines.push(line);
-      }
+      });
     });
   };
 
   /** 设置面板缩放 */
   const initPanZoom = () => {
+    console.log('initPanZoom');
     const mainContainer = jsPlumbInstance.getContainer();
     const mainContainerWrap = mainContainer.parentNode;
     // @ts-ignore
@@ -215,8 +205,11 @@ const FlowEditor: React.FC = () => {
   };
 
   /** 初始化编辑器面板 */
-  const initPanel = async () => {
+  const initPanel = () => {
+    console.log('ready');
     jsPlumbInstance.ready(() => {
+      // @ts-ignore
+      jsPlumbInstance.setContainer(document.getElementById('flow'));
       // 导入默认配置
       jsPlumbInstance.importDefaults(jsplumbSetting);
       // 连线创建成功后，维护本地数据
@@ -235,7 +228,6 @@ const FlowEditor: React.FC = () => {
       jsPlumbInstance.bind('dblclick', (line) => {
         console.log(line);
         // confirmDeleteLine(line);
-        console.log(auxiliaryLine);
       });
       //断开连线后，维护本地数据
       jsPlumbInstance.bind('connectionDetached', (evt) => {
@@ -295,6 +287,8 @@ const FlowEditor: React.FC = () => {
 
   /** 添加节点 */
   const addNode = (temp: API.Node) => {
+    console.log('addNode: ');
+    console.log(temp);
     data.nodeList.push(temp);
     jsPlumbInstance.makeSource(temp.id, jsplumbSourceOptions);
     jsPlumbInstance.makeTarget(temp.id, jsplumbTargetOptions);
@@ -493,20 +487,11 @@ const FlowEditor: React.FC = () => {
   };
 
   useEffect(() => {
-    (async function initFunction() {
-      await initNodeType();
-      await initFlowData();
-      await initPanel();
-    })();
-
-    // 绑定节点键盘事件
-    document.getElementById('toolbar')?.addEventListener('keyup', (e) => keyupNode(e));
-
-    return () => {
-      if (jsPlumbInstance) {
-        jsPlumbInstance.reset();
-      }
-    };
+    const res1 = initNodeType();
+    const res2 = initFlowData();
+    Promise.all([res1, res2]).then(() => {
+      initPanel();
+    });
   }, []);
 
   return (
