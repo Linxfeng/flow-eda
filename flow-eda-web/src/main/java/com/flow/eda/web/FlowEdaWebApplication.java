ed5d7d7fa0819f7867321e86d6f52007ff667fa9
Linxfeng <1538063845@qq.com>
2022-05-24 15:41:42 +0800

    更新Feign配置

--- flow-eda-web/src/main/java/com/flow/eda/web/FlowEdaWebApplication.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/FlowEdaWebApplication.java
@@ -1,17 +1,14 @@
 package com.flow.eda.web;
 
-import com.flow.eda.web.flow.status.FlowInfoServiceImpl;
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableDubbo
+@EnableFeignClients
 @SpringBootApplication
 @EnableTransactionManagement
-@DubboComponentScan(basePackageClasses = FlowInfoServiceImpl.class)
 @ComponentScan(basePackages = {"com.flow.eda.web", "com.flow.eda.common.config"})
 public class FlowEdaWebApplication {
 
