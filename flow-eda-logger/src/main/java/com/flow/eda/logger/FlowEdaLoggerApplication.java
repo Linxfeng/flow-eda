6c89ddc689aa61d679d3acaa4517bc515737b4d0
Linxfeng <1538063845@qq.com>
2022-05-24 14:28:15 +0800

    去除dubbo和nacos配置

--- flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
@@ -1,15 +1,10 @@
 package com.flow.eda.logger;
 
-import com.flow.eda.logger.logs.LogsServiceImpl;
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@EnableDubbo
-@DubboComponentScan(basePackageClasses = LogsServiceImpl.class)
 @SpringBootApplication
 public class FlowEdaLoggerApplication {
 
