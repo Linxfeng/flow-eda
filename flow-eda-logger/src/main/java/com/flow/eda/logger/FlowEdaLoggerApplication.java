07cb10a98455511ba2e971f206c5510d0628e6d2
Linxfeng <1538063845@qq.com>
2022-05-24 15:35:06 +0800

    更新Feign配置

--- flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
@@ -2,12 +2,10 @@ package com.flow.eda.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.flow.eda.logger", "com.flow.eda.common.config"})
 public class FlowEdaLoggerApplication {
