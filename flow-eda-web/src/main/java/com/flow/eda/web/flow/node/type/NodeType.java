bfaf5ed01d90c8fc76409cf754162873ab3338a1
Linxfeng <1538063845@qq.com>
2022-05-25 16:17:01 +0800

    节点类型 新增menu字段，用于分类

--- flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeType.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/flow/node/type/NodeType.java
@@ -13,6 +13,8 @@ public class NodeType {
     private String type;
     /** 节点类型名称 */
     private String typeName;
+    /** 菜单，用于分栏 */
+    private String menu;
     /** 节点图标 */
     private String svg;
     /** 节点背景色 */
