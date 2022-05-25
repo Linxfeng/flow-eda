0b69582c9b2fccbc421124cad575e0d21d9b3b8a
Linxfeng <1538063845@qq.com>
2022-05-25 19:07:52 +0800

    新增自定义DispatcherServlet，用于动态处理HTTP请求

--- flow-eda-runner/src/main/java/com/flow/eda/runner/FlowEdaRunnerApplication.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/FlowEdaRunnerApplication.java
@@ -1,11 +1,16 @@
 package com.flow.eda.runner;
 
+import com.flow.eda.runner.node.http.response.HttpResponseServlet;
 import com.flow.eda.runner.status.FlowNodeWebsocket;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @EnableDubbo
 @DubboComponentScan(basePackages = "com.flow.eda.runner.data")
@@ -16,5 +21,11 @@ public class FlowEdaRunnerApplication {
         ConfigurableApplicationContext applicationContext =
                 SpringApplication.run(FlowEdaRunnerApplication.class, args);
         FlowNodeWebsocket.setApplicationContext(applicationContext);
+    }
+
+    @Bean
+    @Qualifier(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+    public DispatcherServlet dispatcherServlet() {
+        return new HttpResponseServlet();
     }
 }
---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/http/response/HttpResponseServlet.java
@@ -0,0 +1,43 @@
+package com.flow.eda.runner.node.http.response;
+
+import com.flow.eda.common.exception.FlowException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+/** 自定义DispatcherServlet */
+@Slf4j
+public class HttpResponseServlet extends DispatcherServlet {
+    /** 用于存储HttpResponseNode节点的URI和对应的处理器 */
+    private static final Map<String, HttpResponseNode> HANDLER_MAPPINGS = new HashMap<>();
+
+    public static void addHandlerMapping(HttpResponseNode node) {
+        if (HANDLER_MAPPINGS.containsKey(node.getMapping())) {
+            throw new FlowException("The same params 'uri' and 'method' node already exists");
+        }
+        HANDLER_MAPPINGS.put(node.getMapping(), node);
+    }
+
+    @Override
+    protected void doDispatch(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        HttpResponseNode handler = getHttpResponseHandler(request);
+        if (handler != null) {
+            handler.handle(request, response);
+        } else {
+            super.doDispatch(request, response);
+        }
+    }
+
+    private HttpResponseNode getHttpResponseHandler(HttpServletRequest request) {
+        String mapping = request.getRequestURI() + ":" + request.getMethod();
+        if (HANDLER_MAPPINGS.containsKey(mapping)) {
+            return HANDLER_MAPPINGS.get(mapping);
+        }
+        return null;
+    }
+}
