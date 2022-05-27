8e3e14f0e94bc3d4638ac2fedae74695a649410d
Linxfeng <1538063845@qq.com>
2022-05-27 22:16:26 +0800

    新增节点参数校验方法requiredUrl，用于校验url路径

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/NodeVerify.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/NodeVerify.java
@@ -1,6 +1,7 @@
 package com.flow.eda.runner.node;
 
 import com.flow.eda.common.exception.InvalidParameterException;
+import org.bson.Document;
 
 /** 节点参数校验工具 */
 public class NodeVerify {
@@ -29,6 +30,14 @@ public class NodeVerify {
         if (!condition) {
             throwWithName(name);
         }
+    }
+
+    public static String requiredUrl(Document params, String url) {
+        NodeVerify.notNull(params, url);
+        String uri = params.getString(url);
+        NodeVerify.notBlank(uri, url);
+        NodeVerify.isTrue(uri.startsWith("/"), url);
+        return uri;
     }
 
     public static void throwWithName(String name) {
