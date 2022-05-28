3f8705c732d84629034376d08257a4c6ff116999
Linxfeng <1538063845@qq.com>
2022-05-28 22:14:37 +0800

    更新WebSocket客户端节点

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/client/WsClientNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/client/WsClientNode.java
@@ -3,24 +3,43 @@ package com.flow.eda.runner.node.ws.client;
 import com.flow.eda.runner.node.AbstractNode;
 import com.flow.eda.runner.node.NodeFunction;
 import com.flow.eda.runner.node.NodeVerify;
+import lombok.Setter;
 import org.bson.Document;
 
+/** WebSocket客户端节点 */
+@Setter
 public class WsClientNode extends AbstractNode {
     private String path;
-    private String sendAfterConnected;
+    private String query;
+    private String sendAfterConnect;
     private boolean fanout = false;
+    private NodeFunction callback;
 
     public WsClientNode(Document params) {
         super(params);
     }
 
     @Override
-    public void run(NodeFunction callback) {}
+    public void run(NodeFunction callback) {
+        setStatus(Status.RUNNING);
+        this.callback = callback;
+
+        // 上游节点输出参数发送
+
+    }
 
     @Override
     protected void verify(Document params) {
-        this.path = NodeVerify.requiredUrl(params, "path");
-        this.sendAfterConnected = params.getString("sendAfterConnected");
+        String uri = NodeVerify.requiredUrl(params, "path");
+
+        if (uri.contains("?")) {
+            this.path = uri.split("\\?")[0];
+            this.query = uri.split("\\?")[1];
+        } else {
+            this.path = uri;
+        }
+
+        this.sendAfterConnect = params.getString("sendAfterConnect");
 
         String isSend = params.getString("fanout");
         if ("Send".equals(isSend)) {
