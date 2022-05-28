20cc576aeaa38ca4eceffcf6f97457434851a929
Linxfeng <1538063845@qq.com>
2022-05-28 17:38:11 +0800

    更新WsNodeHandler，向指定path路径的所有客户端session发送消息

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsNodeHandler.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsNodeHandler.java
@@ -11,17 +11,36 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /** 自定义WebSocketHandler，用于处理ws_server节点 */
 @Slf4j
 public class WsNodeHandler extends AbstractWebSocketHandler {
+    /** 管理path路径和session的对应关系 */
+    private static final Map<String, List<WebSocketSession>> SESSION_MAP =
+            new ConcurrentHashMap<>();
 
     public WsNodeHandler() {
         super();
+    }
+
+    /** 向指定客户端session发送消息 */
+    public static void sendMessage(WebSocketSession session, String message) {
+        if (session != null) {
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (Exception e) {
+                log.error("WsNodeHandler: Send websocket message failed:{}", e.getMessage());
+            }
+        }
+    }
+
+    /** 向指定path路径的所有客户端session发送消息 */
+    public static void sendMessage(String path, String message) {
+        if (SESSION_MAP.containsKey(path)) {
+            SESSION_MAP.get(path).forEach(s -> sendMessage(s, message));
+        }
     }
 
     /** 获取ws请求路径 */
@@ -32,12 +51,13 @@ public class WsNodeHandler extends AbstractWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         String path = getPath(session);
+        this.addSession(session);
         log.info("WsNodeHandler: Client connected, path: {}", path);
 
         // 建立连接后发送消息
         WsServerNode node = WsServerNodeManager.getNodeInstance(path);
         Map<String, Object> kv = this.parseQuery(session, node.getQuery());
-        sendMessageReplacePlaceholder(session, node.getSendAfterConnected(), kv);
+        sendMessageReplacePlaceholder(session, node.getSendAfterConnect(), kv);
     }
 
     @Override
@@ -52,36 +72,27 @@ public class WsNodeHandler extends AbstractWebSocketHandler {
 
             // 收到消息后发送消息
             Map<String, Object> kv = this.parseQuery(session, node.getQuery());
-            sendMessageReplacePlaceholder(session, node.getSendAfterReceived(), kv);
+            sendMessageReplacePlaceholder(session, node.getSendAfterReceive(), kv);
         } else {
             throw new IllegalStateException("Unexpected WebSocket message type: " + message);
         }
     }
 
     @Override
-    public void handleTransportError(WebSocketSession session, Throwable exception)
-            throws Exception {
-        log.error("websocket session {} onError:{}", session.getId(), exception.getMessage());
+    public void handleTransportError(WebSocketSession session, Throwable exception) {
+        log.error(
+                "WsNodeHandler: Websocket path: {} onError: {}",
+                getPath(session),
+                exception.getMessage());
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status)
-            throws Exception {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        this.removeSession(session);
         log.info(
-                "Client disconnected, session id:{}, CloseStatus:{}",
-                session.getId(),
-                status.getCode());
-    }
-
-    /** 向指定客户端session发送消息 */
-    public void sendMessage(WebSocketSession session, String message) {
-        if (session != null) {
-            try {
-                session.sendMessage(new TextMessage(message));
-            } catch (Exception e) {
-                log.error("WsNodeHandler: Send websocket message failed:{}", e.getMessage());
-            }
-        }
+                "WsNodeHandler: Client disconnected, path: {}, Reason: {}",
+                getPath(session),
+                status.getReason());
     }
 
     /** 发送指定消息，解析并替换占位符 */
@@ -114,5 +125,23 @@ public class WsNodeHandler extends AbstractWebSocketHandler {
             }
         }
         return kv;
+    }
+
+    private void addSession(WebSocketSession session) {
+        String path = getPath(session);
+        if (SESSION_MAP.containsKey(path)) {
+            SESSION_MAP.get(path).add(session);
+        } else {
+            List<WebSocketSession> list = new ArrayList<>();
+            list.add(session);
+            SESSION_MAP.put(path, list);
+        }
+    }
+
+    private void removeSession(WebSocketSession session) {
+        String path = getPath(session);
+        if (SESSION_MAP.containsKey(path)) {
+            SESSION_MAP.get(path).remove(session);
+        }
     }
 }
