3e634855a62335573af80c02ba4ee82d963e1fb4
Linxfeng <1538063845@qq.com>
2022-05-28 17:38:53 +0800

    更新WS服务端节点 输出参数

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsServerNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsServerNode.java
@@ -4,6 +4,7 @@ import com.flow.eda.common.exception.FlowException;
 import com.flow.eda.runner.node.AbstractNode;
 import com.flow.eda.runner.node.NodeFunction;
 import com.flow.eda.runner.node.NodeVerify;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.springframework.util.StringUtils;
@@ -12,12 +13,15 @@ import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
+@Getter
 public class WsServerNode extends AbstractNode {
     private String path;
     private List<String> query;
-    private String sendAfterConnected;
-    private String sendAfterReceived;
+    private String sendAfterConnect;
+    private String sendAfterReceive;
     private boolean fanout = false;
+    private NodeFunction callback;
+    private Document output;
 
     public WsServerNode(Document params) {
         super(params);
@@ -26,8 +30,18 @@ public class WsServerNode extends AbstractNode {
     @Override
     public void run(NodeFunction callback) {
         setStatus(Status.RUNNING);
+        this.callback = callback;
         WsServerNodeManager.addEndpoint(this);
-        callback.callback(output());
+
+        // 上游节点输出参数发送
+        Document input = getInput();
+        if (!input.isEmpty()) {
+            output.append("params", input);
+        }
+        output.putAll(output());
+        output.remove("input");
+        output.remove("payload");
+        WsNodeHandler.sendMessage(path, output.toJson());
     }
 
     @Override
@@ -37,38 +51,31 @@ public class WsServerNode extends AbstractNode {
         if (path.contains("{") || path.contains("}")) {
             throw new FlowException("The parameter 'path' can not contains symbols '{' or '}'");
         }
+        // set output
+        this.output = new Document();
+        this.output.putAll(params);
+        this.output.remove("path");
 
         String q = params.getString("query");
         if (StringUtils.hasLength(q)) {
             this.query = Arrays.asList(q.split(","));
         }
+        this.output.remove("query");
 
-        this.sendAfterConnected = params.getString("sendAfterConnected");
-        this.sendAfterReceived = params.getString("sendAfterReceived");
+        this.sendAfterConnect = params.getString("sendAfterConnect");
+        this.output.remove("sendAfterConnect");
+
+        this.sendAfterReceive = params.getString("sendAfterReceive");
+        this.output.remove("sendAfterReceive");
 
         String isSend = params.getString("fanout");
         if ("Send".equals(isSend)) {
             this.fanout = true;
         }
+        this.output.remove("fanout");
     }
 
-    public String getPath() {
-        return path;
-    }
-
-    public List<String> getQuery() {
-        return query;
-    }
-
-    public String getSendAfterConnected() {
-        return sendAfterConnected;
-    }
-
-    public String getSendAfterReceived() {
-        return sendAfterReceived;
-    }
-
-    public boolean isFanout() {
-        return fanout;
+    public void callback(String message) {
+        this.callback.callback(output().append("message", message));
     }
 }
