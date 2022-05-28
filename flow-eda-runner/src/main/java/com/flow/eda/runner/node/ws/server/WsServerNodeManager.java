ebd62cd8db076f8ecd01adf8c10bdabc5eec9ef6
Linxfeng <1538063845@qq.com>
2022-05-28 15:32:52 +0800

    新增WS服务端节点管理器

---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsServerNodeManager.java
@@ -0,0 +1,54 @@
+package com.flow.eda.runner.node.ws.server;
+
+import com.flow.eda.common.exception.FlowException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/** WS服务端节点管理器 */
+public class WsServerNodeManager {
+
+    /** 管理节点path对应的节点实例 */
+    private static final Map<String, WsServerNode> PATH_NODE = new ConcurrentHashMap<>();
+    /** 管理节点id和节点path的对应关系 */
+    private static final Map<String, String> ID_PATH = new ConcurrentHashMap<>();
+
+    /**
+     * 添加一个ws Endpoint
+     *
+     * @param node ws服务端节点实例
+     */
+    public static void addEndpoint(WsServerNode node) {
+        String path = node.getPath();
+        // 先校验是否已存在相同path
+        if (containsPath(path)) {
+            // 是否存在相同实例：若存在则更新节点实例，若不存在则抛出异常
+            if (ID_PATH.containsKey(node.getNodeId())) {
+                PATH_NODE.put(path, node);
+                return;
+            }
+            throw new FlowException("The path '" + path + "' same node already exists");
+        }
+        PATH_NODE.put(path, node);
+        ID_PATH.put(node.getNodeId(), path);
+    }
+
+    /**
+     * 移除一个ws Endpoint
+     *
+     * @param node ws服务端节点实例
+     */
+    public static void removeEndpoint(WsServerNode node) {
+        PATH_NODE.remove(node.getPath());
+        ID_PATH.remove(node.getNodeId());
+    }
+
+    public static boolean containsPath(String path) {
+        return PATH_NODE.containsKey(path);
+    }
+
+    /** 根据path获取节点实例 */
+    public static WsServerNode getNodeInstance(String path) {
+        return PATH_NODE.get(path);
+    }
+}
