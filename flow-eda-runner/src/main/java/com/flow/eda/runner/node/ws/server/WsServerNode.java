c67e394d40da4fa1c990365aa10ba47070195578
Linxfeng <1538063845@qq.com>
2022-05-28 15:33:22 +0800

    更新WS服务端节点参数校验

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsServerNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/server/WsServerNode.java
@@ -1,16 +1,20 @@
 package com.flow.eda.runner.node.ws.server;
 
+import com.flow.eda.common.exception.FlowException;
 import com.flow.eda.runner.node.AbstractNode;
 import com.flow.eda.runner.node.NodeFunction;
 import com.flow.eda.runner.node.NodeVerify;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
-@Getter
 public class WsServerNode extends AbstractNode {
     private String path;
+    private List<String> query;
     private String sendAfterConnected;
     private String sendAfterReceived;
     private boolean fanout = false;
@@ -22,12 +26,23 @@ public class WsServerNode extends AbstractNode {
     @Override
     public void run(NodeFunction callback) {
         setStatus(Status.RUNNING);
+        WsServerNodeManager.addEndpoint(this);
         callback.callback(output());
     }
 
     @Override
     protected void verify(Document params) {
+        // 校验路径参数，不支持通配符
         this.path = NodeVerify.requiredUrl(params, "path");
+        if (path.contains("{") || path.contains("}")) {
+            throw new FlowException("The parameter 'path' can not contains symbols '{' or '}'");
+        }
+
+        String q = params.getString("query");
+        if (StringUtils.hasLength(q)) {
+            this.query = Arrays.asList(q.split(","));
+        }
+
         this.sendAfterConnected = params.getString("sendAfterConnected");
         this.sendAfterReceived = params.getString("sendAfterReceived");
 
@@ -35,5 +50,25 @@ public class WsServerNode extends AbstractNode {
         if ("Send".equals(isSend)) {
             this.fanout = true;
         }
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public List<String> getQuery() {
+        return query;
+    }
+
+    public String getSendAfterConnected() {
+        return sendAfterConnected;
+    }
+
+    public String getSendAfterReceived() {
+        return sendAfterReceived;
+    }
+
+    public boolean isFanout() {
+        return fanout;
     }
 }
