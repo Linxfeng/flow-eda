d8a653230b3662922531119a8b73708925e99929
Linxfeng <1538063845@qq.com>
2022-05-28 21:49:36 +0800

    提供比较两个阻塞节点是否相同的扩展方法

--- flow-eda-runner/src/main/java/com/flow/eda/runner/runtime/FlowBlockNodePool.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/runtime/FlowBlockNodePool.java
@@ -11,7 +11,10 @@ public class FlowBlockNodePool {
 
     public static void addBlockNode(String flowId, BlockNode blockNode) {
         if (POOL.containsKey(flowId)) {
-            POOL.get(flowId).add(blockNode);
+            List<BlockNode> list = POOL.get(flowId);
+            // 若判断两个阻塞节点相同，需要进行替换更新
+            list.removeIf(node -> node.eq(blockNode));
+            list.add(blockNode);
         } else {
             List<BlockNode> list = new ArrayList<>();
             list.add(blockNode);
@@ -29,5 +32,10 @@ public class FlowBlockNodePool {
     /** 阻塞节点需要实现此接口，重写销毁方法 */
     public interface BlockNode {
         void destroy();
+
+        /** 提供比较两个阻塞节点是否相同的扩展方法 */
+        default boolean eq(BlockNode blockNode) {
+            return false;
+        }
     }
 }
