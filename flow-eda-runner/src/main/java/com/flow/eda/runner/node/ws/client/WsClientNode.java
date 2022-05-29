dc5ee7484e85ea56c7d5a2d2a93028bd27c96a1c
Linxfeng <1538063845@qq.com>
2022-05-29 13:32:12 +0800

    更新WebSocket客户端节点，运行时创建ws客户端连接

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/client/WsClientNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/ws/client/WsClientNode.java
@@ -1,18 +1,19 @@
 package com.flow.eda.runner.node.ws.client;
 
+import com.flow.eda.common.exception.FlowException;
 import com.flow.eda.runner.node.AbstractNode;
 import com.flow.eda.runner.node.NodeFunction;
 import com.flow.eda.runner.node.NodeVerify;
-import lombok.Setter;
+import lombok.Getter;
 import org.bson.Document;
+import org.springframework.util.StringUtils;
 
 /** WebSocket客户端节点 */
-@Setter
+@Getter
 public class WsClientNode extends AbstractNode {
     private String path;
     private String query;
     private String sendAfterConnect;
-    private boolean fanout = false;
     private NodeFunction callback;
 
     public WsClientNode(Document params) {
@@ -21,29 +22,42 @@ public class WsClientNode extends AbstractNode {
 
     @Override
     public void run(NodeFunction callback) {
+        // 同一节点不可重复运行
+        if (WsClientNodeManager.contains(getNodeId())) {
+            throw new FlowException("The ws_client node can not run repeatedly");
+        }
         setStatus(Status.RUNNING);
         this.callback = callback;
 
-        // 上游节点输出参数发送
-
+        // 创建ws客户端连接
+        WsClientNodeManager.createWebSocketClient(this);
     }
 
     @Override
     protected void verify(Document params) {
         String uri = NodeVerify.requiredUrl(params, "path");
-
         if (uri.contains("?")) {
             this.path = uri.split("\\?")[0];
-            this.query = uri.split("\\?")[1];
+            String q = uri.split("\\?")[1];
+            if (StringUtils.hasText(q) && q.contains("=")) {
+                this.query = q;
+            }
         } else {
             this.path = uri;
         }
 
         this.sendAfterConnect = params.getString("sendAfterConnect");
+    }
 
-        String isSend = params.getString("fanout");
-        if ("Send".equals(isSend)) {
-            this.fanout = true;
+    /** 回调，收到消息后向下游节点输出 */
+    public void callback(String message) {
+        this.callback.callback(output().append("message", message));
+    }
+
+    public String getUri() {
+        if (query != null) {
+            return path + "?" + query;
         }
+        return path;
     }
 }
