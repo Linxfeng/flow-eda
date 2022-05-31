382f37189ba19f1b5c18108c24a9bec5cf50f976
Linxfeng <1538063845@qq.com>
2022-05-31 10:44:33 +0800

    修改样式，缩短菜单栏宽度

--- flow-eda-react/config/config.ts
+++ flow-eda-react/config/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   layout: {
     // https://umijs.org/zh-CN/plugins/plugin-layout
     locale: true,
-    siderWidth: 208,
+    siderWidth: 188,
     ...defaultSettings,
   },
   // https://umijs.org/zh-CN/plugins/plugin-locale
