9bbab0a038bc59e72832981776c81e5068560f80
Linxfeng <1538063845@qq.com>
2022-06-01 16:17:50 +0800

    MQTT订阅节点新增校验重复连接

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/mqtt/sub/MqttSubNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/mqtt/sub/MqttSubNode.java
@@ -3,9 +3,9 @@ package com.flow.eda.runner.node.mqtt.sub;
 import com.flow.eda.common.exception.FlowException;
 import com.flow.eda.runner.node.NodeFunction;
 import com.flow.eda.runner.node.mqtt.MqttAbstractNode;
+import com.flow.eda.runner.node.mqtt.MqttClientManager;
 import com.flow.eda.runner.runtime.FlowBlockNodePool;
 import org.bson.Document;
-import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 /** MQTT订阅节点 */
@@ -18,17 +18,25 @@ public class MqttSubNode extends MqttAbstractNode implements FlowBlockNodePool.B
 
     @Override
     public void run(NodeFunction callback) {
+        // 校验是否重复连接
+        if (MqttClientManager.contains(this)) {
+            throw new FlowException("The mqtt_sub node can not run repeatedly");
+        }
+        if (MqttClientManager.contains(getClientId())) {
+            throw new FlowException(
+                    "The clientId '" + getClientId() + "' same node already exists");
+        }
 
         setStatus(Status.RUNNING);
         this.callback = callback;
 
         try {
             // 建立MQTT连接
-            MqttClient client = createMqttClient();
+            MqttClientManager.createMqttClient(this);
             // 设置回调
-            client.setCallback(new MqttMessageCallback(this));
+            getClient().setCallback(new MqttMessageCallback(this));
             // 订阅topic
-            client.subscribe(getTopic());
+            getClient().subscribe(getTopic());
         } catch (MqttException e) {
             throw new FlowException(e.getMessage());
         }
@@ -47,6 +55,6 @@ public class MqttSubNode extends MqttAbstractNode implements FlowBlockNodePool.B
     @Override
     public void destroy() {
         // 断开MQTT客户端连接，关闭会话
-        super.closeMqttClient();
+        MqttClientManager.closeMqttClient(this);
     }
 }
