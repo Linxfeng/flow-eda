b1779d58f3b8e43286d28ebc9783b9e2eff6d007
Linxfeng <1538063845@qq.com>
2022-06-01 11:14:10 +0800

    新增MQTT订阅节点

---
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/mqtt/sub/MqttSubNode.java
@@ -0,0 +1,45 @@
+package com.flow.eda.runner.node.mqtt.sub;
+
+import com.flow.eda.common.exception.FlowException;
+import com.flow.eda.runner.node.NodeFunction;
+import com.flow.eda.runner.node.mqtt.MqttAbstractNode;
+import org.bson.Document;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+/** MQTT订阅节点 */
+public class MqttSubNode extends MqttAbstractNode {
+    private NodeFunction callback;
+
+    public MqttSubNode(Document params) {
+        super(params);
+    }
+
+    @Override
+    public void run(NodeFunction callback) {
+
+        setStatus(Status.RUNNING);
+        this.callback = callback;
+
+        try {
+            // 建立MQTT连接
+            MqttClient client = createMqttClient();
+            // 设置回调
+            client.setCallback(new MqttMessageCallback(this));
+            // 订阅topic
+            client.subscribe(getTopic());
+        } catch (MqttException e) {
+            throw new FlowException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void verify(Document params) {
+        super.verify(params);
+    }
+
+    /** 回调，收到消息后向下游节点输出 */
+    public void callback(String message) {
+        this.callback.callback(output().append("message", message));
+    }
+}
