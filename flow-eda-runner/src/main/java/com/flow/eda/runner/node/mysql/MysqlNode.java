6c0195ef9bdc05c66a9b85e00b1e99f01d147882
Linxfeng <1538063845@qq.com>
2022-06-04 17:37:39 +0800

    更新mysql节点，支持执行多条语句，开启事务异常后回滚

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/mysql/MysqlNode.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/mysql/MysqlNode.java
@@ -30,41 +30,36 @@ public class MysqlNode extends AbstractNode {
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
             // 建立数据库连接
             connection = DriverManager.getConnection(url, username, password);
-            // 执行sql语句
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet resultSet = statement.executeQuery();
-            List<Document> list = new ArrayList<>();
-            while (resultSet.next()) {
-                Document result = new Document();
-                ResultSetMetaData metaData = resultSet.getMetaData();
-                int columnCount = metaData.getColumnCount();
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    Object value = resultSet.getObject(i);
-                    result.append(columnName, value);
-                }
-                list.add(result);
-            }
-            // 自动提交，开启事务？
 
-            // 关闭statement
-            resultSet.close();
-            statement.close();
+            // 开启事务
+            connection.setAutoCommit(false);
+            Document result = new Document();
+
+            // 执行sql语句
+            if (batchSql != null) {
+                for (int i = 0; i < batchSql.size(); i++) {
+                    String s = batchSql.get(i);
+                    Object resData = this.executeQuery(connection, s);
+                    result.append("sql-" + (i + 1), s).append("result-" + (i + 1), resData);
+                }
+            } else {
+                Object resData = this.executeQuery(connection, this.sql);
+                result.append("sql", this.sql).append("result", resData);
+            }
+            // 提交事务
+            connection.commit();
 
             // 输出结果
-            Object resData = null;
-            if (list.size() == 1) {
-                resData = list.get(0);
-            } else if (list.size() > 1) {
-                resData = list;
-            }
-            Document output = output();
-            if (resData != null) {
-                output.append("result", resData);
-            }
-            callback.callback(output);
+            result.putAll(output());
+            callback.callback(result);
         } catch (Exception e) {
-            e.printStackTrace();
+            // 发生异常后回滚
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ignored) {
+                }
+            }
             throw new FlowException(e.getMessage());
         } finally {
             // 关闭连接
@@ -75,6 +70,54 @@ public class MysqlNode extends AbstractNode {
                 }
             }
         }
+    }
+
+    /** 执行sql查询语句 */
+    private Object executeQuery(Connection connection, String sql) throws SQLException {
+        if (!sql.toLowerCase().startsWith("select")) {
+            return this.executeUpdate(connection, sql);
+        }
+        List<Document> list = new ArrayList<>();
+        // 执行sql语句
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+
+        // 解析结果
+        while (resultSet.next()) {
+            Document result = new Document();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                Object value = resultSet.getObject(i);
+                result.append(columnName, value);
+            }
+            list.add(result);
+        }
+
+        // 关闭statement
+        resultSet.close();
+        statement.close();
+
+        // 返回结果
+        Object resData;
+        if (list.isEmpty()) {
+            resData = "{}";
+        } else if (list.size() == 1) {
+            resData = list.get(0);
+        } else {
+            resData = list;
+        }
+        return resData;
+    }
+
+    /** 执行sql更新/插入/删除等语句 */
+    private String executeUpdate(Connection connection, String sql) throws SQLException {
+        // 执行sql语句
+        PreparedStatement statement = connection.prepareStatement(sql);
+        int update = statement.executeUpdate();
+        statement.close();
+        return "Affected rows: " + update;
     }
 
     @Override
