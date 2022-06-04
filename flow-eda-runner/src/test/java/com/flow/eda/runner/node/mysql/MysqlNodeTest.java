347d73b69c77798c74c6ec558d8e4b70322f019c
Linxfeng <1538063845@qq.com>
2022-06-04 18:30:04 +0800

    更新mysql节点单元测试

--- flow-eda-runner/src/test/java/com/flow/eda/runner/node/mysql/MysqlNodeTest.java
+++ flow-eda-runner/src/test/java/com/flow/eda/runner/node/mysql/MysqlNodeTest.java
@@ -8,12 +8,10 @@ class MysqlNodeTest {
     @Test
     void run() {
         Document params = new Document();
-        params.append(
-                "url",
-                "jdbc:mysql://localhost:3306/flow_eda?useSSL=false&useUnicode=true&characterEncoding=utf-8");
+        params.append("url", "jdbc:mysql://localhost:3306/flow_eda");
         params.append("username", "root");
         params.append("password", "123456");
-        params.append("sql", "SELECT id,type,type_name FROM eda_flow_node_type WHERE id IN (1,2)");
+        params.append("sql", "SELECT type,type_name FROM eda_flow_node_type WHERE id IN (1,2)");
         MysqlNode node = new MysqlNode(params);
         node.run(p -> System.out.println(p.toJson()));
     }
