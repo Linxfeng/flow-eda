0643db81f7b0b080d4c745273f7bff124f54ef88
Linxfeng <1538063845@qq.com>
2022-06-08 14:50:03 +0800

    更新mongodb节点单元测试

--- flow-eda-runner/src/test/java/com/flow/eda/runner/node/mongodb/MongodbNodeTest.java
+++ flow-eda-runner/src/test/java/com/flow/eda/runner/node/mongodb/MongodbNodeTest.java
@@ -8,9 +8,12 @@ class MongodbNodeTest {
     @Test
     void run() {
         Document params = new Document();
-        params.append("url", "mongodb://admin:admin@127.0.0.1:27017/admin");
+        params.append("url", "mongodb://root:admin@127.0.0.1:27017/admin");
         params.append("db", "test_db");
-        params.append("command", "db.getCollection('test_coll').find({\"name\" : \"test\"})");
+        params.append("command", "{'find': 'test_coll', 'filter': {'name': 'test'}, 'limit': 1}");
+        params.append(
+                "command1",
+                "{'update': 'test_coll', 'updates': [{'q':{},'u':{'$set':{'test': 'xxx'}}, 'multi': true}]}");
         MongodbNode node = new MongodbNode(params);
         node.run(p -> System.out.println(p.toJson()));
     }
