c7d73a702a2a50eb47aa0743c6c47226c8e26e64
Linxfeng <1538063845@qq.com>
2022-06-08 17:14:15 +0800

    节点类型新增redis节点

--- flow-eda-runner/src/main/java/com/flow/eda/runner/node/NodeTypeEnum.java
+++ flow-eda-runner/src/main/java/com/flow/eda/runner/node/NodeTypeEnum.java
@@ -11,6 +11,7 @@ import com.flow.eda.runner.node.mqtt.sub.MqttSubNode;
 import com.flow.eda.runner.node.mysql.MysqlNode;
 import com.flow.eda.runner.node.output.OutputNode;
 import com.flow.eda.runner.node.parser.ParserNode;
+import com.flow.eda.runner.node.redis.RedisNode;
 import com.flow.eda.runner.node.start.StartNode;
 import com.flow.eda.runner.node.timer.TimerNode;
 import com.flow.eda.runner.node.ws.client.WsClientNode;
@@ -39,6 +40,7 @@ public enum NodeTypeEnum {
     /** 数据库节点 */
     MYSQL("mysql", MysqlNode.class),
     MONGODB("mongodb", MongodbNode.class),
+    REDIS("redis", RedisNode.class),
     ;
     private final String type;
     private final Class<? extends Node> clazz;
