0e0ff54d61e3aafe9d8e8a25293d12329c651505
Linxfeng <1538063845@qq.com>
2022-06-12 16:42:42 +0800

    新增oauth2模块，用于资源认证

---
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/FlowEdaOauth2Application.java
@@ -0,0 +1,12 @@
+package com.flow.eda.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowEdaOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowEdaOauth2Application.class, args);
+    }
+}
---
+++ flow-eda-oauth2/src/test/java/com/flow/eda/oauth2/FlowEdaOauth2ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.flow.eda.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowEdaOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
