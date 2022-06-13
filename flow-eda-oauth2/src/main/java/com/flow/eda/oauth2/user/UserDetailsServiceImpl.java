89cd39c5a981038dbfbb81532b2f22536ba9f23e
Linxfeng <1538063845@qq.com>
2022-06-13 21:49:16 +0800

    新增oauth2用户信息配置

---
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/user/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.flow.eda.oauth2.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<GrantedAuthority> roleUser =
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+        return new User(username, passwordEncoder.encode(username), roleUser);
+    }
+}
