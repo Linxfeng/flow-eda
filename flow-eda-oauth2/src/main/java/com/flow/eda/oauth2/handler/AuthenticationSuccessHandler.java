e72981d3d4cfb91cf8525f05e750e3910ba92d94
Linxfeng <1538063845@qq.com>
2022-06-13 21:48:55 +0800

    新增oauth2认证成功处理器

---
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/handler/AuthenticationSuccessHandler.java
@@ -0,0 +1,90 @@
+package com.flow.eda.oauth2.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+@Slf4j
+@Component
+public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Autowired private ObjectMapper objectMapper;
+    @Autowired private PasswordEncoder passwordEncoder;
+    @Autowired private ClientDetailsService clientDetailsService;
+    @Autowired private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
+        }
+        String[] tokens = this.extractAndDecodeHeader(header, request);
+
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId 对应的配置信息不存在" + clientId);
+        } else if (!passwordEncoder.matches(clientSecret, clientDetails.getClientSecret())) {
+            throw new UnapprovedClientAuthenticationException("clientSecret 不匹配" + clientId);
+        }
+
+        TokenRequest tokenRequest =
+                new TokenRequest(new HashMap<>(), clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication =
+                new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken token =
+                authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+        String s = objectMapper.writeValueAsString(token);
+        response.getWriter().write(s);
+        log.info("token={}", s);
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+        byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+        String token = new String(decoded, StandardCharsets.UTF_8);
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[] {token.substring(0, delim), token.substring(delim + 1)};
+        }
+    }
+}
