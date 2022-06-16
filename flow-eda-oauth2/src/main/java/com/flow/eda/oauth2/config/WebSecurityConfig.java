f43adc1b31996ee6c2e350b8626f20c863250dc6
Linxfeng <1538063845@qq.com>
2022-06-16 21:21:59 +0800

    更新oauth2 WebSecurityConfig

--- flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/config/WebSecurityConfig.java
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/config/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package com.flow.eda.oauth2.config;
 
+import com.flow.eda.oauth2.user.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Lazy @Autowired private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     @Override
@@ -31,11 +41,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests()
-                // .antMatchers("/oauth/**").permitAll()
+        http.cors()
                 .and()
                 .csrf()
-                .disable();
+                .disable()
+                .authorizeRequests()
+                // 处理跨域请求中的Preflight请求
+                .antMatchers(HttpMethod.OPTIONS)
+                .permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest)
+                .permitAll()
+                .antMatchers("/oauth/**")
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .deleteCookies("JSESSIONID")
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.eraseCredentials(true);
     }
 }
--- flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/user/UserDetailsServiceImpl.java
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/user/UserDetailsServiceImpl.java
@@ -8,10 +8,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired private PasswordEncoder passwordEncoder;
