8fbc43d46e45f15ade82a5949ec95878090be646
Linxfeng <1538063845@qq.com>
2022-06-18 17:09:16 +0800

    新增登陆/登出/成功/失败处理器

--- flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/TestOauth2Controller.java
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/TestOauth2Controller.java
@@ -1,4 +1,4 @@
-package com.flow.eda.oauth2.handler;
+package com.flow.eda.oauth2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/handler/LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.flow.eda.oauth2.handler;
+
+import com.flow.eda.common.http.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/** 登录成功处理器 */
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        response.getWriter().write(Result.success().toJson());
+    }
+}
