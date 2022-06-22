3b11241b5bfb6915af5dd15b233b1a70bf9196c2
Linxfeng <1538063845@qq.com>
2022-06-22 18:15:56 +0800

    配置本地API请求代理

--- flow-eda-vue/vite.config.js
+++ flow-eda-vue/vite.config.js
@@ -1,15 +1,19 @@
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 export default {
-    server: {
-        port: 8080,
-        proxy: {
-            '^/api/v1': {
-                target: `http://localhost:8081`,
-                ws: true,
-                changeOrigin: true
-            }
-        }
+  server: {
+    port: 8080,
+    proxy: {
+      "^/api/v1": {
+        target: `http://localhost:8081`,
+        ws: true,
+        changeOrigin: true,
+      },
+      "^/oauth": {
+        target: `http://localhost:8086`,
+        changeOrigin: true,
+      },
     },
-    plugins: [vue()]
-}
+  },
+  plugins: [vue()],
+};
