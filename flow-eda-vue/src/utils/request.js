bb3587d517796f93dc6bcc2636147cbcdb25d00d
Linxfeng <1538063845@qq.com>
2022-06-22 18:15:36 +0800

    更新request组件，弹出API返回的错误信息

--- flow-eda-vue/src/utils/request.js
+++ flow-eda-vue/src/utils/request.js
@@ -1,41 +1,46 @@
-import axios from 'axios'
-import {ElMessage} from "element-plus";
+import axios from "axios";
+import { ElMessage } from "element-plus";
 
 const service = axios.create({
-    timeout: 8000,
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
-    },
-    withCredentials: true
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
 });
 
 service.interceptors.request.use(
-    config => {
-        return config;
-    },
-    error => {
-        console.log(error);
-        return Promise.reject();
-    }
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
 );
 
 service.interceptors.response.use(
-    response => {
-        const res = response.data;
-        if (res.message) {
-            ElMessage.error(res.message);
-        } else {
-            return res;
-        }
-    },
-    error => {
-        if (error.response) {
-            ElMessage.error(error.response.data.error);
-        } else {
-            ElMessage.error("request timeout");
-        }
+  (response) => {
+    const res = response.data;
+    if (res.message) {
+      ElMessage.error(res.message);
+    } else {
+      return res;
     }
+  },
+  (error) => {
+    if (error.response) {
+      const res = error.response.data;
+      if (res.message) {
+        ElMessage.error(res.message);
+      } else if (res.error) {
+        ElMessage.error(res.error);
+      }
+    } else {
+      ElMessage.error("request timeout");
+    }
+  }
 );
 
 export default service;
