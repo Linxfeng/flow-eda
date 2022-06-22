516fafeacb097f82522f304250992957d459fcb4
Linxfeng <1538063845@qq.com>
2022-06-22 16:24:54 +0800

    更新用户登陆页面

--- flow-eda-vue/src/views/Login.vue
+++ flow-eda-vue/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-body">
+  <div class="login-body" v-loading="loading">
     <div class="login-container">
       <div class="head">
         <div class="name">
@@ -9,29 +9,42 @@
       <el-form
         label-position="top"
         :rules="rules"
-        :model="ruleForms"
-        ref="loginForm"
+        :model="ruleForm"
+        ref="loginFormRef"
         class="login-form"
       >
-        <el-form-item label="用户名：" prop="username">
+        <el-form-item label="账号：" prop="username">
           <el-input
             type="text"
-            v-model.trim="ruleForms.username"
+            v-model="ruleForm.username"
             autocomplete="off"
+            @keyup.enter.native="handleLogin(loginFormRef)"
           ></el-input>
         </el-form-item>
         <el-form-item label="密码：" prop="password">
           <el-input
             type="password"
-            v-model.trim="ruleForms.password"
+            v-model="ruleForm.password"
             autocomplete="off"
+            @keyup.enter.native="handleLogin(loginFormRef)"
+            show-password
           ></el-input>
         </el-form-item>
         <el-form-item>
-          <div style="color: #333">登录表示您已同意<a>《服务条款》</a></div>
-          <el-button style="width: 100%" type="primary" @click="handleLogin"
-            >立即登录</el-button
-          >
+          <el-button
+            style="width: 100%; margin-bottom: 10px"
+            type="primary"
+            @click="handleLogin(loginFormRef)"
+            >立即登录
+          </el-button>
+          <p class="tips">
+            提示：默认使用体验账号 test 登录，体验账号的数据会每日重置。
+          </p>
+          <p class="tips">
+            若需要永久保存用户所有数据，请先<a class="href" href="/"
+              >注册账号</a
+            >
+          </p>
         </el-form-item>
       </el-form>
     </div>
@@ -39,10 +52,9 @@
 </template>
 
 <script>
-import { reactive, computed } from "vue";
+import { ref, reactive, computed } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { ElMessage } from "element-plus";
 import { getToken } from "../api/oauth2";
 
 export default {
@@ -51,10 +63,12 @@ export default {
     const router = useRouter();
     const store = useStore();
 
-    const ruleForms = reactive({ username: "", password: "" });
+    const loginFormRef = ref(null);
+    const loading = ref(false);
+    const ruleForm = reactive({ username: "test", password: "test" });
     const rules = {
       username: [
-        { required: "true", message: "账户不能为空", trigger: "blur" },
+        { required: "true", message: "账号不能为空", trigger: "blur" },
       ],
       password: [
         { required: "true", message: "密码不能为空", trigger: "blur" },
@@ -62,29 +76,35 @@ export default {
     };
     const tagsList = computed(() => store.state.tagsList);
 
-    // 进入登录页，先刷新以关闭其他标签页
-    if (tagsList.value.length > 1) {
-      location.reload();
-    }
-
     // 用户登录
-    const handleLogin = () => {
-      getToken(ruleForms.username, ruleForms.password).then((res) => {
-        if (res) {
-          localStorage.setItem("flow.user", JSON.stringify(res));
-          const item = tagsList.value[0];
-          // 登陆成功，跳转首页
-          store.commit("delTagsItem", { item });
-          router.push("/");
-        } else {
-          ElMessage.error("login failed");
+    const handleLogin = (formEl) => {
+      formEl.validate((valid) => {
+        if (valid) {
+          loading.value = true;
+          getToken(ruleForm.username, ruleForm.password).then((res) => {
+            if (res) {
+              if (res.access_token) {
+                localStorage.setItem("access_token", res.access_token);
+              }
+              if (res.refresh_token) {
+                localStorage.setItem("refresh_token", res.refresh_token);
+              }
+              // 登陆成功，跳转首页
+              const item = tagsList.value[0];
+              store.commit("delTagsItem", { item });
+              router.push("/");
+            }
+            loading.value = false;
+          });
         }
       });
     };
 
     return {
+      loading,
       rules,
-      ruleForms,
+      ruleForm,
+      loginFormRef,
       handleLogin,
     };
   },
@@ -97,8 +117,8 @@ export default {
   align-items: center;
   justify-content: center;
   width: 100%;
+  height: 100%;
   background-color: #fff;
-  /* background-image: linear-gradient(25deg, #077f7c, #3aa693, #5ecfaa, #7ffac2); */
 }
 
 .login-container {
@@ -123,9 +143,9 @@ export default {
 }
 
 .head .title {
-  color: #1baeae;
+  color: #409eff;
   font-weight: bold;
-  font-size: 28px;
+  font-size: 26px;
 }
 
 .head {
@@ -137,9 +157,23 @@ export default {
   width: 70%;
   margin: 0 auto;
 }
+
+.tips {
+  margin-top: 12px;
+  line-height: 24px;
+}
+
+.href {
+  color: #409eff;
+  font-weight: bold;
+  font-size: 16px;
+  text-decoration: underline;
+}
 </style>
+
 <style>
 .login-form {
   margin-bottom: 12px;
+  color: #606266;
 }
 </style>
