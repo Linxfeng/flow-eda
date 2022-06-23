56751261495d7fa06bd55b1123d84a58e7a3baec
Linxfeng <1538063845@qq.com>
2022-06-23 18:42:33 +0800

    配置oauth2权限认证服务dubbo接口

--- flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/FlowEdaOauth2Application.java
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/FlowEdaOauth2Application.java
@@ -1,10 +1,15 @@
 package com.flow.eda.oauth2;
 
+import com.flow.eda.oauth2.config.Oauth2ScopeServiceImpl;
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDubbo
 @SpringBootApplication
+@DubboComponentScan(basePackageClasses = Oauth2ScopeServiceImpl.class)
 @ComponentScan(basePackages = {"com.flow.eda.oauth2", "com.flow.eda.common.config"})
 public class FlowEdaOauth2Application {
 
