7bf31eb207aec01d81b2c012541869f3104d4776
Linxfeng <1538063845@qq.com>
2022-06-24 11:12:27 +0800

    新增刷新token接口，完善登录/注册接口

--- flow-eda-vue/src/api/oauth2.js
+++ flow-eda-vue/src/api/oauth2.js
@@ -25,29 +25,24 @@ async function getBasicInfo() {
   return "Basic " + btoa(clientId + ":" + clientSecret);
 }
 
-/**用户登录*/
-export async function userLogin(username, password) {
-  const params = new URLSearchParams();
-  params.append("grant_type", "password");
-  params.append("username", username);
-  params.append("password", password);
+/**获取token*/
+async function getOauthToken(data) {
   const basic = await getBasicInfo();
-  request({
+  const res = await request({
     url: "/oauth/token",
     method: "post",
     headers: {
       Authorization: basic,
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    data: params,
-  }).then((res) => {
-    if (res && res.access_token && res.refresh_token) {
-      localStorage.setItem("access_token", res.access_token);
-      localStorage.setItem("refresh_token", res.refresh_token);
-      return true;
-    }
-    return false;
+    data: data,
   });
+  if (res && res.access_token && res.refresh_token) {
+    localStorage.setItem("access_token", res.access_token);
+    localStorage.setItem("refresh_token", res.refresh_token);
+    return true;
+  }
+  return false;
 }
 
 /**用户注册*/
@@ -59,19 +54,23 @@ export function userRegister(body) {
   });
 }
 
+/**用户登录*/
+export async function userLogin(username, password) {
+  const params = new URLSearchParams();
+  params.append("grant_type", "password");
+  params.append("username", username);
+  params.append("password", password);
+  return await getOauthToken(params);
+}
+
 /**刷新token*/
 export async function refreshToken() {
-  const params = new URLSearchParams();
-  params.append("grant_type", "refresh_token");
-  params.append("refresh_token", localStorage.getItem("refresh_token"));
-  const basic = await getBasicInfo();
-  return request({
-    url: "/oauth/token",
-    method: "post",
-    headers: {
-      Authorization: basic,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    data: params,
-  });
+  const refreshToken = localStorage.getItem("refresh_token");
+  if (refreshToken) {
+    const params = new URLSearchParams();
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", refreshToken);
+    return await getOauthToken(params);
+  }
+  return false;
 }
