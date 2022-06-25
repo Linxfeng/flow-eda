021e6a8983d1fa5bf5c275aa09f6ca1a2416334f
Linxfeng <1538063845@qq.com>
2022-06-25 13:08:00 +0800

    配置为资源服务器，通过Oauth2认证鉴权

--- flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
+++ flow-eda-logger/src/main/java/com/flow/eda/logger/FlowEdaLoggerApplication.java
@@ -6,11 +6,13 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @EnableDubbo
 @DubboComponentScan(basePackageClasses = LogsServiceImpl.class)
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.flow.eda.logger", "com.flow.eda.common.resource"})
 public class FlowEdaLoggerApplication {
 
     public static void main(String[] args) {
