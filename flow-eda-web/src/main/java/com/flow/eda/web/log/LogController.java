b9e3e06e0def3e4e25371069ade41e8fdaf8b58c
Linxfeng <1538063845@qq.com>
2022-06-26 16:07:31 +0800

    操作日志不允许删除

--- flow-eda-web/src/main/java/com/flow/eda/web/log/LogController.java
+++ flow-eda-web/src/main/java/com/flow/eda/web/log/LogController.java
@@ -1,6 +1,8 @@
 package com.flow.eda.web.log;
 
+import com.flow.eda.common.dubbo.api.LogsService;
 import com.flow.eda.common.dubbo.model.Logs;
+import com.flow.eda.common.exception.InvalidVerifyTokenException;
 import com.flow.eda.common.exception.MissingRequestParameterException;
 import com.flow.eda.common.http.Result;
 import com.flow.eda.common.utils.CollectionUtil;
@@ -8,18 +10,25 @@ import com.flow.eda.web.http.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class LogController {
+    private static final String ADMIN = "admin";
     @Autowired private LogService logService;
 
     @OperationLog
     @GetMapping("/logs")
-    public PageResult<Logs> logList(LogRequest request) {
+    public PageResult<Logs> logList(LogRequest request, Principal principal) {
         if (request.getType() == null) {
             throw new MissingRequestParameterException("type");
+        }
+        // 仅管理员用户可查看操作日志
+        if (LogsService.Type.OPERATION.equals(request.getType())
+                && !ADMIN.equals(principal.getName())) {
+            throw new InvalidVerifyTokenException("Insufficient permissions");
         }
         return PageResult.ofPage(logService.getLogList(request), request);
     }
@@ -27,8 +36,10 @@ public class LogController {
     @OperationLog
     @DeleteMapping("/logs")
     public Result<String> deleteLogs(@RequestBody List<String> ids) {
-        if (CollectionUtil.isNotEmpty(ids)) {
-            logService.deleteLogs(ids);
+        // 操作日志不允许删除
+        List<String> list = CollectionUtil.filter(ids, id -> !id.startsWith("/logs/operation"));
+        if (CollectionUtil.isNotEmpty(list)) {
+            logService.deleteLogs(list);
         }
         return Result.ok();
     }
