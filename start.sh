220fef9913aa5cb43a4d1cfb7a6ef255cf8f2652
Linxfeng <1538063845@qq.com>
2022-07-20 11:12:29 +0800

    更新docker启用命令

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -d -p 8086:8086 flow-eda-oauth2
-docker run -d -p 8082:8082 flow-eda-logger
+docker run -d -p 8082:8082 -v /root/app/springboot/logger/logs:/logs flow-eda-logger
 docker run -d -p 8088:8088 flow-eda-runner
 docker run -d -p 8081:8081 flow-eda-web
