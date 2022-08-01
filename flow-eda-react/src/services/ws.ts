9430bd79bfe86b5ba6f4fdfe6a09c297ff6ad964
Linxfeng <1538063845@qq.com>
2022-08-01 14:57:25 +0800

    封装WebSocket请求自动刷新token

--- flow-eda-react/src/services/ws.ts
+++ flow-eda-react/src/services/ws.ts
@@ -1,12 +1,32 @@
 import { wsIp } from '@/global';
+import { refreshToken } from '@/services/api';
+import { history } from '@@/core/history';
+import { message } from 'antd';
 
 const nodeWs = {};
 const logWs = {};
 const logContentWs = {};
 
 function openWs(url: string, callback: (res: string) => void) {
+  const urlWs = wsIp + url + '?access_token=';
   // websocket请求url带上token认证
-  const socket = new WebSocket(url + '?access_token=' + localStorage.getItem('access_token'));
+  const socket = new WebSocket(urlWs + localStorage.getItem('access_token'));
+  socket.onerror = async function () {
+    // 刷新token
+    const pass = await refreshToken();
+    if (pass) {
+      // 刷新token成功，重新之前的请求
+      const ws = new WebSocket(urlWs + localStorage.getItem('access_token'));
+      ws.onmessage = function (msg) {
+        callback(msg.data);
+      };
+      return ws;
+    }
+    // 登录过期，跳转登陆页
+    await message.error('登录过期', 1);
+    history.push('/login');
+    return null;
+  };
   socket.onmessage = function (msg) {
     callback(msg.data);
   };
@@ -16,7 +36,7 @@ function openWs(url: string, callback: (res: string) => void) {
 /** 监听流程运行时的节点信息 */
 export function onOpenNode(id: string, callback: (res: string) => void) {
   if (Object.keys(nodeWs).length === 0 || !nodeWs[id]) {
-    const url = wsIp + ':8088/ws/flow/' + id + '/nodes';
+    const url = ':8088/ws/flow/' + id + '/nodes';
     nodeWs[id] = openWs(url, callback);
   }
 }
@@ -24,7 +44,7 @@ export function onOpenNode(id: string, callback: (res: string) => void) {
 /** 监听流程运行时的日志信息 */
 export function onOpenLogs(id: string, callback: (res: string) => void) {
   if (Object.keys(logWs).length === 0 || !logWs[id]) {
-    const url = wsIp + ':8082/ws/flow/' + id + '/logs';
+    const url = ':8082/ws/flow/' + id + '/logs';
     logWs[id] = openWs(url, callback);
   }
 }
@@ -32,7 +52,7 @@ export function onOpenLogs(id: string, callback: (res: string) => void) {
 /** 接收日志文件内容 */
 export function onOpenLogContent(path: string, callback: (res: string) => void) {
   if (Object.keys(logContentWs).length === 0 || !logContentWs[path]) {
-    const url = wsIp + ':8082/ws/logs/content/' + path.replaceAll('/', ':');
+    const url = ':8082/ws/logs/content/' + path.replaceAll('/', ':');
     logContentWs[path] = openWs(url, callback);
   }
 }
