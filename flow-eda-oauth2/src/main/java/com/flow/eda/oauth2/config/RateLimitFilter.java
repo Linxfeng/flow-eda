f8a91569557bf3304bd887541bda012caada9348
Linxfeng <1538063845@qq.com>
2022-08-02 19:12:58 +0800

    配置guava令牌桶算法实现限流

--- flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/config/RateLimitFilter.java
+++ flow-eda-oauth2/src/main/java/com/flow/eda/oauth2/config/RateLimitFilter.java
@@ -1,64 +1,33 @@
 package com.flow.eda.oauth2.config;
 
-import com.flow.eda.common.http.Result;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /** 自定义限流过滤器 */
-//@Component
-public class RateLimitFilter implements Filter {
-    /** 同个IP每秒最大访问量 */
-    private static final Integer MAX_VISIT = 3;
-    /** 请求IP对应的请求次数 */
-    private final Map<String, Integer> visitMap = new ConcurrentHashMap<>(4);
+@Component
+public class RateLimitFilter extends OncePerRequestFilter {
+    /** guava令牌桶算法实现限流，每秒5个请求 */
+    private final RateLimiter limiter = RateLimiter.create(5.0);
 
     @Override
-    public void init(FilterConfig filterConfig) {}
-
-    @Override
-    public void doFilter(
-            ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        if (servletRequest instanceof HttpServletRequest) {
-            HttpServletRequest request = (HttpServletRequest) servletRequest;
-            String ip = this.getIp(request);
-            int visitCount = 1;
-            if (this.visitMap.containsKey(ip)) {
-                visitCount = this.visitMap.get(ip) + 1;
-                if (visitCount >= MAX_VISIT) {
-                    this.forbiddenRequest(servletResponse);
-                    return;
-                }
-            }
-            this.visitMap.put(ip, visitCount);
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        // 获取不到令牌，立即进入服务降级
+        if (!limiter.tryAcquire()) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Rate limit exceeded");
+            // throw new AccessDeniedException("Rate limit exceeded");
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
-
-    /** 禁止访问 */
-    private void forbiddenRequest(ServletResponse servletResponse) throws IOException {
-        if (servletResponse instanceof HttpServletResponse) {
-            HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setStatus(HttpStatus.FORBIDDEN.value());
-            response.getWriter()
-                    .write(Result.failed("Server is busy! Please try again later").toJson());
-        }
-    }
-
-    private String getIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Real-IP");
-        return ip != null ? ip : request.getRemoteAddr();
-    }
-
-    @Override
-    public void destroy() {}
 }
