c1fcc6e6b010631b080837666dd87d8ff11cf4ec
Linxfeng <1538063845@qq.com>
2022-08-18 14:14:51 +0800

    新增获取版本列表接口

--- flow-eda-react/src/services/api.ts
+++ flow-eda-react/src/services/api.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import type { Key } from 'react';
 import { request } from 'umi';
+import type { Key } from 'react';
 
 /** 获取流程列表 */
 export async function getFlowList(params: any) {
@@ -72,6 +72,14 @@ export async function getNodeTypes() {
 /** 获取流程节点数据 */
 export async function getFlowData(id: string) {
   return request<API.ApiResult>('/api/v1/node/data', {
+    method: 'GET',
+    params: { flowId: id },
+  });
+}
+
+/** 获取流程数据版本 */
+export async function getVersion(id: string) {
+  return request<API.ApiResult>('/api/v1/node/data/version', {
     method: 'GET',
     params: { flowId: id },
   });
