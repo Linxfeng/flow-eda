387ce8fcadd89a6800b69066d3a570503afd5027
aborn <loveaborn@foxmail.com>
2022-12-22 17:52:30 +0800

    feat:升级element-plus&element-theme-chalk到最新版本

--- flow-eda-vue/src/main.js
+++ flow-eda-vue/src/main.js
@@ -1,7 +1,7 @@
 import vClickOutside from "click-outside-vue3";
 import ElementPlus from "element-plus";
 import localeZH from "element-plus/lib/locale/lang/zh-cn";
-import "element-plus/lib/theme-chalk/index.css";
+import "element-plus/theme-chalk/index.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/iconfont/iconfont.css";
