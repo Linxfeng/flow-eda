851f46f161fc99e26fe098d85a283059b7f7cc08
aborn <loveaborn@foxmail.com>
2023-01-04 20:22:11 +0800

    feat:回车即搜索

--- flow-eda-vue/src/views/Flows.vue
+++ flow-eda-vue/src/views/Flows.vue
@@ -6,6 +6,7 @@
           v-model="params.name"
           class="handle-input mr10"
           placeholder="名称"
+          @change="handleSearch"
         />
         <el-select
           v-model="params.status"
